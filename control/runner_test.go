c9c4a077da6032103ebb59657f3e3ac1468c179c
Nicholas Weaver <lynxbat@gmail.com>
2015-01-12 17:54:10 -0800

    SDI-43 #comment adds delegate addition, start, stop, empty func for future handling.

---
+++ control/runner_test.go
@@ -0,0 +1,166 @@
+package control
+
+import (
+	"github.com/intelsdilabs/gomit"
+
+	"errors"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+type MockHandlerDelegate struct {
+	ErrorMode       bool
+	WasRegistered   bool
+	WasUnregistered bool
+	StopError       error
+}
+
+func (m *MockHandlerDelegate) RegisterHandler(s string, h gomit.Handler) error {
+	if m.ErrorMode {
+		return errors.New("fake delegate error")
+	}
+	m.WasRegistered = true
+	return nil
+}
+
+func (m *MockHandlerDelegate) UnregisterHandler(s string) error {
+	if m.StopError != nil {
+		return m.StopError
+	}
+	m.WasUnregistered = true
+	return nil
+}
+
+func (m *MockHandlerDelegate) HandlerCount() int {
+	return 0
+}
+
+func (m *MockHandlerDelegate) IsHandlerRegistered(s string) bool {
+	return false
+}
+
+// Uses the dummy collector plugin to simulate Loading
+func TestRunner(t *testing.T) {
+	Convey("pulse/control", t, func() {
+
+		Convey("Runner", func() {
+
+			Convey(".AddDelegates", func() {
+
+				Convey("adds a handler delegate", func() {
+					r := new(Runner)
+
+					r.AddDelegates(new(MockHandlerDelegate))
+					So(len(r.delegates), ShouldEqual, 1)
+				})
+
+				Convey("adds multiple delegates", func() {
+					r := new(Runner)
+
+					r.AddDelegates(new(MockHandlerDelegate))
+					r.AddDelegates(new(MockHandlerDelegate))
+					So(len(r.delegates), ShouldEqual, 2)
+				})
+
+				Convey("adds multiple delegates (batch)", func() {
+					r := new(Runner)
+
+					r.AddDelegates(new(MockHandlerDelegate), new(MockHandlerDelegate))
+					So(len(r.delegates), ShouldEqual, 2)
+				})
+
+			})
+
+			Convey(".Start", func() {
+
+				Convey("Starting Runner without adding delegates", func() {
+					r := new(Runner)
+					e := r.Start()
+
+					So(e, ShouldNotBeNil)
+					So(e.Error(), ShouldEqual, "No delegates added before called Start()")
+				})
+
+				Convey("Starting Runner after adding one delegates", func() {
+					r := new(Runner)
+					m1 := new(MockHandlerDelegate)
+					r.AddDelegates(m1)
+					e := r.Start()
+
+					So(e, ShouldBeNil)
+					So(m1.WasRegistered, ShouldBeTrue)
+				})
+
+				Convey("Starting Runner after adding multiple delegates", func() {
+					r := new(Runner)
+					m1 := new(MockHandlerDelegate)
+					m2 := new(MockHandlerDelegate)
+					m3 := new(MockHandlerDelegate)
+					r.AddDelegates(m1)
+					r.AddDelegates(m2, m3)
+					e := r.Start()
+
+					So(e, ShouldBeNil)
+					So(m1.WasRegistered, ShouldBeTrue)
+					So(m2.WasRegistered, ShouldBeTrue)
+					So(m3.WasRegistered, ShouldBeTrue)
+				})
+
+				Convey("Throws error if delegate cannot RegisterHandler", func() {
+					r := new(Runner)
+					me := new(MockHandlerDelegate)
+					me.ErrorMode = true
+					r.AddDelegates(me)
+					e := r.Start()
+
+					So(e, ShouldNotBeNil)
+					So(e.Error(), ShouldEqual, "fake delegate error")
+				})
+
+			})
+
+			Convey(".Stop", func() {
+
+				Convey("removes handlers from delegates", func() {
+					r := new(Runner)
+					m1 := new(MockHandlerDelegate)
+					m2 := new(MockHandlerDelegate)
+					m3 := new(MockHandlerDelegate)
+					r.AddDelegates(m1)
+					r.AddDelegates(m2, m3)
+					r.Start()
+					e := r.Stop()
+
+					So(m1.WasUnregistered, ShouldBeTrue)
+					So(m2.WasUnregistered, ShouldBeTrue)
+					So(m3.WasUnregistered, ShouldBeTrue)
+					// No errors
+					So(len(e), ShouldEqual, 0)
+				})
+
+				Convey("returns errors for handlers errors on stop", func() {
+					r := new(Runner)
+					m1 := new(MockHandlerDelegate)
+					m1.StopError = errors.New("0")
+					m2 := new(MockHandlerDelegate)
+					m2.StopError = errors.New("1")
+					m3 := new(MockHandlerDelegate)
+					r.AddDelegates(m1)
+					r.AddDelegates(m2, m3)
+					r.Start()
+					e := r.Stop()
+
+					So(m1.WasUnregistered, ShouldBeFalse)
+					So(m2.WasUnregistered, ShouldBeFalse)
+					So(m3.WasUnregistered, ShouldBeTrue)
+					// No errors
+					So(len(e), ShouldEqual, 2)
+					So(e[0].Error(), ShouldEqual, "0")
+					So(e[1].Error(), ShouldEqual, "1")
+				})
+
+			})
+
+		})
+	})
+}
