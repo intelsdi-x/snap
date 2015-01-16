1b8703bdd110ab016d9dccafa2930a1eab17fa1d
geauxvirtual <justin.guidroz@gmail.com>
2015-01-16 15:39:42 -0600

    Add TestLoadPlugin test

--- control/plugin_manager_test.go
+++ control/plugin_manager_test.go
@@ -1,1 +1,45 @@
 package control
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	PluginName = "pulse-collector-dummy"
+	PulsePath  = os.Getenv("PULSE_PATH")
+	PluginPath = path.Join(PulsePath, "plugin", "collector", PluginName)
+)
+
+// Uses the dummy collector plugin to simulate loading
+func TestLoadPlugin(t *testing.T) {
+	// These tests only work if PULSE_PATH is known
+	// It is the responsibility of the testing framework to
+	// build the plugins first into the build dir
+
+	if PulsePath != "" {
+		Convey("PluginManager.LoadPlugin", t, func() {
+
+			Convey("loads plugin successfully", func() {
+				p := PluginManager()
+				p.Start()
+				err := p.LoadPlugin(PluginPath)
+
+				So(p.LoadedPlugins, ShouldNotBeEmpty)
+				So(err, ShouldBeNil)
+			})
+
+			Convey("returns error if PluginManager is not started", func() {
+				p := PluginManager()
+				err := p.LoadPlugin(PluginPath)
+
+				So(p.LoadedPlugins, ShouldBeEmpty)
+				So(err, ShouldNotBeNil)
+			})
+		})
+
+	}
+}
