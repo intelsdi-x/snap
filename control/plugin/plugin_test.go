26dfce444fb658270118ed7e013b3d1b4c15566d
dan pittman <danielscottt@gmail.com>
2015-01-22 10:35:31 -0800

    SDI-81: PluginCatalog

---
+++ control/plugin/plugin_test.go
@@ -0,0 +1,16 @@
+package plugin
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPluginType(t *testing.T) {
+	Convey(".String()", t, func() {
+		Convey("it returns the correct string representation", func() {
+			p := PluginType(0)
+			So(p.String(), ShouldEqual, "collector")
+		})
+	})
+}
