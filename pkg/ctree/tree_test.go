68207b1a242ec45ceccc422c9247e2bd3e0616f7
Nicholas Weaver <lynxbat@gmail.com>
2015-02-20 14:49:15 -0800

    Better test names because computers

--- pkg/ctree/tree_test.go
+++ pkg/ctree/tree_test.go
@@ -35,101 +35,102 @@ func TestConfigTree(t *testing.T) {
 	})
 
 	Convey("Get()", t, func() {
-		fmt.Println("")
-		d1 := newDummyNode()
-		d1.data = "a"
-		d2 := newDummyNode()
-		d2.data = "b"
-		d3 := newDummyNode()
-		d3.data = "c"
-		d4 := newDummyNode()
-		d4.data = "d"
-		c := New()
-		c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
-		c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan"}, d2)
-		c.Add([]string{"intel", "foo", "manhole", "joel", "dan"}, d3)
-		c.Add([]string{"intel", "foo", "manhole", "joel", "dan", "mark"}, d4)
-		c.Freeze()
-		c.print()
-		g := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
-		fmt.Println(g.(*dummyNode).data)
-	})
+		Convey("order preserved", func() {
+			d1 := newDummyNode()
+			d1.data = "a"
+			d2 := newDummyNode()
+			d2.data = "b"
+			d3 := newDummyNode()
+			d3.data = "c"
+			d4 := newDummyNode()
+			d4.data = "d"
+			c := New()
+			c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
+			c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan"}, d2)
+			c.Add([]string{"intel", "foo", "manhole", "joel", "dan"}, d3)
+			c.Add([]string{"intel", "foo", "manhole", "joel", "dan", "mark"}, d4)
+			c.Freeze()
+			g := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
+			So(g.(*dummyNode).data, ShouldResemble, "b/a")
+			So(g, ShouldNotBeNil)
+		})
 
-	Convey("Get2()", t, func() {
-		c := New()
-		c.Freeze()
-		n := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
-		So(n, ShouldBeNil)
-	})
+		Convey("blank tree return nil", func() {
+			c := New()
+			c.Freeze()
+			n := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
+			So(n, ShouldBeNil)
+		})
 
-	Convey("Get3()", t, func() {
-		c := New()
-		d1 := newDummyNode()
-		d1.data = "a"
-		c.Add([]string{"intel"}, d1)
-		c.Freeze()
-		n := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
-		So(n, ShouldBeNil)
-	})
+		Convey("long ns short tree", func() {
+			c := New()
+			d1 := newDummyNode()
+			d1.data = "a"
+			c.Add([]string{"intel"}, d1)
+			c.Freeze()
+			n := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
+			So(n, ShouldBeNil)
+		})
 
-	Convey("Get4()", t, func() {
-		c := New()
-		d1 := newDummyNode()
-		d1.data = "a"
-		c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
-		c.Freeze()
-		n := c.Get([]string{"intel"})
-		So(n, ShouldBeNil)
-	})
+		Convey("long tree short ns", func() {
+			c := New()
+			d1 := newDummyNode()
+			d1.data = "a"
+			c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
+			c.Freeze()
+			n := c.Get([]string{"intel"})
+			So(n, ShouldBeNil)
+		})
 
-	Convey("Get5()", t, func() {
-		fmt.Println("")
-		d1 := newDummyNode()
-		d1.data = "a"
-		d2 := newDummyNode()
-		d2.data = "b"
-		c := New()
-		c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
-		c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan"}, d2)
-		c.Freeze()
-		c.print()
-		g := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
-		fmt.Println(g.(*dummyNode).data)
-	})
+		Convey("basic get", func() {
+			d1 := newDummyNode()
+			d1.data = "a"
+			d2 := newDummyNode()
+			d2.data = "b"
+			c := New()
+			c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
+			c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan"}, d2)
+			c.Freeze()
+			g := c.Get([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"})
+			So(g, ShouldNotBeNil)
+		})
 
-	Convey("Get6()", t, func() {
-		fmt.Println("")
-		d1 := newDummyNode()
-		d1.data = "a"
-		d2 := newDummyNode()
-		d2.data = "b"
-		c := New()
-		c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
-		c.Add([]string{"intel", "foo", "sdilabs"}, d2)
-		c.Freeze()
-		c.print()
-		g := c.Get([]string{"intel", "foo", "sdilabs", "joel"})
-		fmt.Println(g.(*dummyNode).data)
-	})
+		Convey("get is inbetween two nodes in tree", func() {
+			d1 := newDummyNode()
+			d1.data = "a"
+			d2 := newDummyNode()
+			d2.data = "b"
+			c := New()
+			c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
+			c.Add([]string{"intel", "foo", "sdilabs"}, d2)
+			c.Freeze()
+			g := c.Get([]string{"intel", "foo", "sdilabs", "joel"})
+			So(g, ShouldNotBeNil)
+		})
 
-	Convey("Get6()", t, func() {
-		d1 := newDummyNode()
-		d1.data = "a"
-		d2 := newDummyNode()
-		d2.data = "b"
-		c := New()
-		c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
+		Convey("adding a new root panics", func() {
+			d1 := newDummyNode()
+			d1.data = "a"
+			d2 := newDummyNode()
+			d2.data = "b"
+			c := New()
+			c.Add([]string{"intel", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d1)
 
-		So(func() {
-			c.Add([]string{"mashery", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d2)
-		}, ShouldPanic)
-	})
+			So(func() {
+				c.Add([]string{"mashery", "foo", "sdilabs", "joel", "dan", "nick", "justin", "sarah"}, d2)
+			}, ShouldPanic)
+		})
 
-	Convey("Get7()", t, func() {
-		d1 := newDummyNode()
-		d1.data = "a"
-		c := New()
-		c.Add([]string{}, d1)
+		Convey("doesn't panic on empty ns", func() {
+			d1 := newDummyNode()
+			d1.data = "a"
+			c := New()
+			So(func() {
+				c.Add([]string{}, d1)
+				g := c.Get([]string{})
+				So(g, ShouldBeNil)
+			}, ShouldNotPanic)
+		})
 	})
 
 	Convey("Frozen()", t, func() {
