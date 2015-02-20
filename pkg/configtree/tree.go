f55db794a04900784284dd37cf3025d07f13054a
dan pittman <danielscottt@gmail.com>
2015-02-19 16:50:36 -0800

    trees

---
+++ pkg/configtree/tree.go
@@ -0,0 +1,53 @@
+package ctree
+
+import (
+	"sync"
+)
+
+type ConfigTree struct {
+	root  *node
+	mutex *sync.Mutex
+}
+
+func NewConfigTree() *ConfigTree {
+	return &ConfigTree{
+		root:  &node{},
+		mutex: &sync.Mutex{},
+	}
+}
+
+func (ct *ConfigTree) Add(ns []string, node Node) {
+	f, remain := ns[0], ns[1:]
+	if ct.root == nil {
+		ct.root = &node{
+			key: f,
+		}
+		ct.root.add(remain)
+	} else {
+	}
+}
+
+func (ct *ConfigTree) Get() Node {}
+
+type Node interface {
+}
+
+type node struct {
+	nodes *node
+	Node  Node
+}
+
+func (n *node) add(ns []string) {
+	f, remain := ns[0], ns[1:]
+	for _, nd := range n.nodes {
+		if f == nd.key {
+			nd.add(remain)
+			return
+		}
+	}
+	newNode := &node{
+		key: f,
+	}
+	newNode.add(remain)
+	n.nodes = append(n.nodes, newNode)
+}
