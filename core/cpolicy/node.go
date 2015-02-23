56c4d83283bd094945d81aca768de340d4fb5118
Nicholas Weaver <lynxbat@gmail.com>
2015-02-23 15:58:11 -0800

    Almost done with node

--- core/cpolicy/node.go
+++ core/cpolicy/node.go
@@ -1,26 +1,97 @@
 package cpolicy
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 
+	"github.com/intelsdilabs/pulse/core/ctypes"
 	"github.com/intelsdilabs/pulse/pkg/ctree"
 )
+
+type ProcessingErrors struct {
+	errors []error
+	mutex  *sync.Mutex
+}
+
+func newProcessingErrors() *ProcessingErrors {
+	return &ProcessingErrors{
+		errors: []error{},
+		mutex:  &sync.Mutex{},
+	}
+}
+
+func (p *ProcessingErrors) Errors() []error {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	return p.errors
+}
+
+func (p *ProcessingErrors) HasErrors() bool {
+	return len(p.errors) > 0
+}
+
+func (p *ProcessingErrors) AddError(e error) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	p.errors = append(p.errors, e)
+}
 
 type ConfigPolicyNode struct {
 	rules map[string]Rule
 	mutex *sync.Mutex
 }
 
+func NewPolicyNode() *ConfigPolicyNode {
+	return &ConfigPolicyNode{
+		rules: make(map[string]Rule),
+		mutex: &sync.Mutex{},
+	}
+}
+
 // Adds a rule to this policy node
-func (p *ConfigPolicyNode) Add(r Rule) {
+func (p *ConfigPolicyNode) Add(rules ...Rule) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	p.rules[r.Key()] = r
+	for _, r := range rules {
+		p.rules[r.Key()] = r
+	}
 }
 
 // Validates and returns a processed policy node or nil and error if validation has failed
-func (c *ConfigPolicyNode) Process() (*ConfigPolicyNode, error) {
-	return nil, nil
+func (c *ConfigPolicyNode) Process(m map[string]ctypes.ConfigValue) (*map[string]ctypes.ConfigValue, *ProcessingErrors) {
+	pErrors := newProcessingErrors()
+	// Loop through each rule and process
+	for key, rule := range c.rules {
+		// items exists for rule
+		if cv, ok := m[key]; ok {
+			println(cv)
+
+			// Validate versus matching data
+			e := rule.Validate(cv)
+			if e != nil {
+				pErrors.AddError(e)
+			}
+		} else {
+			// If it was required add error
+			if rule.Required() {
+				e := errors.New(fmt.Sprintf("required key missing (%s)", key))
+				pErrors.AddError(e)
+			} else {
+				// If default returns we should add it
+				cv := rule.Default()
+				if cv != nil {
+					m[key] = cv
+				}
+
+			}
+		}
+	}
+
+	if pErrors.HasErrors() {
+		return nil, pErrors
+	}
+	return &m, pErrors
 }
 
 // Merges a provided policy node over (overwriting) this one and returns the copy.
--- core/cpolicy/node_test.go
+++ core/cpolicy/node_test.go
@@ -1,1 +1,104 @@
 package cpolicy
+
+import (
+	"testing"
+
+	"github.com/intelsdilabs/pulse/core/ctypes"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// Helpful method to switch to slice of strings for goconvey contains checking
+func errorsMsg(errors []error) []string {
+	s := []string{}
+	for _, e := range errors {
+		s = append(s, e.Error())
+	}
+	return s
+}
+
+func TestConfigPolicyNode(t *testing.T) {
+
+	Convey("returns error for missing required data", t, func() {
+		n := NewPolicyNode()
+		So(n, ShouldNotBeNil)
+
+		m := map[string]ctypes.ConfigValue{}
+		m["username"] = ctypes.ConfigValueStr{Value: "root"}
+
+		r1, _ := NewStringRule("username", true)
+		r2, _ := NewStringRule("password", true)
+
+		n.Add(r1, r2)
+
+		_, pe := n.Process(m)
+
+		So(pe.HasErrors(), ShouldBeTrue)
+		So(len(pe.Errors()), ShouldEqual, 1)
+		So(errorsMsg(pe.Errors()), ShouldContain, "required key missing (password)")
+	})
+
+	Convey("returns errors for missing required data (mutliple)", t, func() {
+		n := NewPolicyNode()
+		So(n, ShouldNotBeNil)
+
+		m := map[string]ctypes.ConfigValue{}
+		m["username"] = ctypes.ConfigValueStr{Value: "root"}
+
+		r1, _ := NewStringRule("username", true)
+		r2, _ := NewStringRule("password", true)
+		r3, _ := NewIntegerRule("port", true)
+
+		n.Add(r1, r2, r3)
+
+		_, pe := n.Process(m)
+
+		So(pe.HasErrors(), ShouldBeTrue)
+		So(len(pe.Errors()), ShouldEqual, 2)
+		So(errorsMsg(pe.Errors()), ShouldContain, "required key missing (port)")
+		So(errorsMsg(pe.Errors()), ShouldContain, "required key missing (password)")
+	})
+
+	Convey("returns error for mismatched type", t, func() {
+		n := NewPolicyNode()
+		So(n, ShouldNotBeNil)
+
+		m := map[string]ctypes.ConfigValue{}
+		m["username"] = ctypes.ConfigValueStr{Value: "root"}
+		m["password"] = ctypes.ConfigValueStr{Value: "password"}
+		m["port"] = ctypes.ConfigValueStr{Value: "8080"}
+
+		r1, _ := NewStringRule("username", true)
+		r2, _ := NewStringRule("password", true)
+		r3, _ := NewIntegerRule("port", true)
+
+		n.Add(r1, r2, r3)
+
+		_, pe := n.Process(m)
+
+		So(len(pe.Errors()), ShouldEqual, 1)
+		So(errorsMsg(pe.Errors()), ShouldContain, "type mismatch (port wanted type 'string' but provided type 'integer')")
+	})
+
+	Convey("adds defaults to only missing values that should have them", t, func() {
+		n := NewPolicyNode()
+		So(n, ShouldNotBeNil)
+
+		m := map[string]ctypes.ConfigValue{}
+		// m["username"] = ctypes.ConfigValueStr{Value: "root"}
+		m["password"] = ctypes.ConfigValueStr{Value: "password"}
+		// m["port"] = ctypes.ConfigValueStr{Value: "8080"}
+
+		r1, _ := NewStringRule("username", false, "root")
+		r2, _ := NewStringRule("password", true)
+		r3, _ := NewIntegerRule("port", false, 8080)
+
+		n.Add(r1, r2, r3)
+
+		m2, pe := n.Process(m)
+
+		So(len(pe.Errors()), ShouldEqual, 0)
+		So((*m2)["username"].(*ctypes.ConfigValueStr).Value, ShouldEqual, "root")
+		So((*m2)["port"].(*ctypes.ConfigValueInt).Value, ShouldEqual, 8080)
+	})
+
+}
