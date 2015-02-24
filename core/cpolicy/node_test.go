f6eade5ac8ecf6dc47a637069aa216c5748fd530
Nicholas Weaver <lynxbat@gmail.com>
2015-02-23 16:04:52 -0800

    Test fixes for node

--- core/cpolicy/node_test.go
+++ core/cpolicy/node_test.go
@@ -84,9 +84,7 @@ func TestConfigPolicyNode(t *testing.T) {
 		So(n, ShouldNotBeNil)
 
 		m := map[string]ctypes.ConfigValue{}
-		// m["username"] = ctypes.ConfigValueStr{Value: "root"}
 		m["password"] = ctypes.ConfigValueStr{Value: "password"}
-		// m["port"] = ctypes.ConfigValueStr{Value: "8080"}
 
 		r1, _ := NewStringRule("username", false, "root")
 		r2, _ := NewStringRule("password", true)
@@ -99,6 +97,24 @@ func TestConfigPolicyNode(t *testing.T) {
 		So(len(pe.Errors()), ShouldEqual, 0)
 		So((*m2)["username"].(*ctypes.ConfigValueStr).Value, ShouldEqual, "root")
 		So((*m2)["port"].(*ctypes.ConfigValueInt).Value, ShouldEqual, 8080)
+	})
+
+	Convey("defaults don't fix missing values on required", t, func() {
+		n := NewPolicyNode()
+		So(n, ShouldNotBeNil)
+
+		m := map[string]ctypes.ConfigValue{}
+		m["password"] = ctypes.ConfigValueStr{Value: "password"}
+
+		r1, _ := NewStringRule("username", true, "root")
+		r2, _ := NewStringRule("password", true)
+
+		n.Add(r1, r2)
+
+		m2, pe := n.Process(m)
+
+		So(len(pe.Errors()), ShouldEqual, 1)
+		So(m2, ShouldBeNil)
 	})
 
 }
