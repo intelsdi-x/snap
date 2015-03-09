85b6e41784d39419c85ddd5b0393ef4d6a8f1c90
Pawel Palucki <pawel.palucki@intel.com>
2015-03-09 09:48:05 -0700

    facter: separate pulse part from facter command

--- plugin/collector/pulse-collector-facter/facter/facter.go
+++ plugin/collector/pulse-collector-facter/facter/facter.go
@@ -1,10 +1,7 @@
 package facter
 
 import (
-	"encoding/json"
-	"errors"
 	"log"
-	"os/exec"
 	"time"
 
 	"github.com/intelsdilabs/pulse/control/plugin"
@@ -226,62 +223,4 @@ func (f *Facter) Collect(args plugin.CollectorArgs, reply *plugin.CollectorReply
 	// }
 
 	return nil
-}
-
-/****************************
- *  external facter cmd api *
- ****************************/
-// helper type to deal with json that stores last update moment
-// for a given fact
-type fact struct {
-	value      interface{}
-	lastUpdate time.Time
-}
-
-// helper type to deal with json parsing
-type stringmap map[string]interface{}
-
-// get facts from facter (external command)
-// returns all keys if none requested
-func getFacts(keys []string, facterTimeout time.Duration) (*stringmap, *time.Time, error) {
-
-	var timestamp time.Time
-
-	// default options
-	args := []string{"--json"}
-	args = append(args, keys...)
-
-	// execute command and capture the output
-	jobCompletedChan := make(chan struct{})
-	timeoutChan := time.After(facterTimeout)
-
-	var err error
-	output := make([]byte, 0, 1024)
-
-	go func(jobCompletedChan chan<- struct{}, output *[]byte, err *error) {
-		*output, *err = exec.Command("facter", args...).Output()
-		jobCompletedChan <- struct{}{}
-	}(jobCompletedChan, &output, &err)
-
-	select {
-	case <-timeoutChan:
-		return nil, nil, errors.New("Facter plugin: fact gathering timeout")
-	case <-jobCompletedChan:
-		// success
-		break
-	}
-
-	if err != nil {
-		log.Println("exec returned " + err.Error())
-		return nil, nil, err
-	}
-	timestamp = time.Now()
-
-	var facterMap stringmap
-	err = json.Unmarshal(output, &facterMap)
-	if err != nil {
-		log.Println("Unmarshal failed " + err.Error())
-		return nil, nil, err
-	}
-	return &facterMap, &timestamp, nil
 }
--- plugin/collector/pulse-collector-facter/facter/facter_test.go
+++ plugin/collector/pulse-collector-facter/facter/facter_test.go
@@ -13,48 +13,6 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestGetFacts(t *testing.T) {
-	Convey("getFacts ", t, func() {
-
-		Convey("time outs", func() {
-			_, _, err := getFacts([]string{}, 0*time.Second)
-			So(err, ShouldNotBeNil)
-		})
-
-		Convey("returns all something within given time", func() {
-			start := time.Now()
-			// 4 seconds because default time for goconvey
-			facts, when, err := getFacts([]string{}, 4*time.Second)
-			So(err, ShouldBeNil)
-			So(facts, ShouldNotBeEmpty)
-			So(*when, ShouldHappenBetween, start, time.Now())
-		})
-
-		Convey("returns right thing when asked eg. kernel => linux", func() {
-			// 4 seconds because default time for goconvey
-			facts, _, err := getFacts([]string{"kernel"}, 4*time.Second)
-			So(err, ShouldBeNil)
-			So(facts, ShouldNotBeEmpty)
-			So(len(*facts), ShouldEqual, 1)
-			fact, exist := (*facts)["kernel"]
-			So(exist, ShouldEqual, true)
-			So(fact, ShouldNotBeNil)
-		})
-
-		Convey("returns nil in fact value when for non existing fact", func() {
-			// 4 seconds because default time for goconvey
-			facts, _, err := getFacts([]string{"thereisnosuchfact"}, 4*time.Second)
-			So(err, ShouldBeNil)
-			So(facts, ShouldNotBeEmpty)
-			So(len(*facts), ShouldEqual, 1)
-			fact, exist := (*facts)["thereisnosuchfact"]
-			So(exist, ShouldEqual, true)
-			So(fact, ShouldBeNil)
-		})
-
-	})
-}
-
 // TODO:
 func TestCacheUpdate(t *testing.T) {
 
