38134406fc597161428836a336584fc44e93d75a
Pawel Palucki <pawel.palucki@intel.com>
2015-03-10 18:24:50 -0700

    facter: clean & test communication with external command

--- plugin/collector/pulse-collector-facter/facter/cmd.go
+++ plugin/collector/pulse-collector-facter/facter/cmd.go
@@ -9,26 +9,43 @@ import (
 	"time"
 )
 
-// configuration variables
-var (
+type cmdConfig struct {
 	// where to find facter executable
 	// tip: somehow golang is able to find executable within PATH, so just a name is enough
 	// even if facter is just a ruby script
-	// overriden durning tests
-	facter_executable = "facter"
-)
+	executable string
+	// default options passed to facter to get json output
+	// overriden during tests to get non parseable output
+	options []string
+}
+
+// overriden durning tests to simulate error
+func newDefaultCmdConfig() *cmdConfig {
+	return &cmdConfig{
+		// tip: somehow golang is able to find executable within PATH, so just a name is enough
+		executable: "facter",
+		options:    []string{"--json"},
+	}
+}
 
 // helper type to deal with json parsing
 type stringmap map[string]interface{}
 
 // get facts from facter (external command)
 // returns all keys if none requested
-func getFacts(keys []string, facterTimeout time.Duration) (*stringmap, *time.Time, error) {
+// if cmdOptions is nil, use defaults "facter --json"
+func getFacts(keys []string, facterTimeout time.Duration, cmdConfig *cmdConfig) (*stringmap, *time.Time, error) {
 
-	var timestamp time.Time
+	// nil means use defaults
+	if cmdConfig == nil {
+		cmdConfig = newDefaultCmdConfig()
+	}
 
-	// default options
-	args := []string{"--json"}
+	// default options -
+	args := make([]string, len(cmdConfig.options))
+	copy(args, cmdConfig.options)
+	// is this more elegant that than this:
+	// > args := append([]string{}, cmdConfig.options...)
 	args = append(args, keys...)
 
 	// execute command and capture the output
@@ -39,7 +56,7 @@ func getFacts(keys []string, facterTimeout time.Duration) (*stringmap, *time.Tim
 	output := make([]byte, 0, 1024)
 
 	go func(jobCompletedChan chan<- struct{}, output *[]byte, err *error) {
-		*output, *err = exec.Command(facter_executable, args...).Output()
+		*output, *err = exec.Command(cmdConfig.executable, args...).Output()
 		jobCompletedChan <- struct{}{}
 	}(jobCompletedChan, &output, &err)
 
@@ -48,19 +65,19 @@ func getFacts(keys []string, facterTimeout time.Duration) (*stringmap, *time.Tim
 		return nil, nil, errors.New("Facter plugin: fact gathering timeout")
 	case <-jobCompletedChan:
 		// success
-		break
 	}
 
 	if err != nil {
-		log.Println("exec returned " + err.Error())
+		log.Println("exec returned ", err.Error())
 		return nil, nil, err
 	}
-	timestamp = time.Now()
+
+	timestamp := time.Now()
 
 	var facterMap stringmap
 	err = json.Unmarshal(output, &facterMap)
 	if err != nil {
-		log.Println("Unmarshal failed " + err.Error())
+		log.Println("Unmarshal failed ", err.Error())
 		return nil, nil, err
 	}
 	return &facterMap, &timestamp, nil
--- plugin/collector/pulse-collector-facter/facter/cmd_test.go
+++ plugin/collector/pulse-collector-facter/facter/cmd_test.go
@@ -9,18 +9,40 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func TestDefaultConfig(t *testing.T) {
+	Convey("check default config", t, func() {
+		cmdConfig := newDefaultCmdConfig()
+		So(cmdConfig.executable, ShouldEqual, "facter")
+		So(cmdConfig.options, ShouldResemble, []string{"--json"})
+	})
+}
+
+func TestCmdCommunication(t *testing.T) {
+	Convey("error when facter binary isn't found", t, func() {
+		_, _, err := getFacts([]string{"whatever"}, DefautlFacterDeadline, &cmdConfig{executable: "wrongbin"})
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldContainSubstring, "file not found") // isn't ubuntu specific ?
+	})
+
+	Convey("error when facter output isn't parsable", t, func() {
+		_, _, err := getFacts([]string{"whatever"}, DefautlFacterDeadline, &cmdConfig{executable: "facter", options: []string{}})
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldContainSubstring, "unexpected end of JSON input")
+	})
+}
+
 func TestGetFacts(t *testing.T) {
 	Convey("getFacts from real facter", t, func() {
 
 		Convey("time outs", func() {
-			_, _, err := getFacts([]string{}, 0*time.Second)
+			_, _, err := getFacts([]string{}, 0*time.Second, nil)
 			So(err, ShouldNotBeNil)
 		})
 
 		Convey("returns all something within given time", func() {
 			start := time.Now()
 			// 4 seconds because default time for goconvey
-			facts, when, err := getFacts([]string{}, 4*time.Second)
+			facts, when, err := getFacts([]string{}, 4*time.Second, nil)
 			So(err, ShouldBeNil)
 			So(facts, ShouldNotBeEmpty)
 			So(*when, ShouldHappenBetween, start, time.Now())
@@ -28,7 +50,7 @@ func TestGetFacts(t *testing.T) {
 
 		Convey("returns right thing when asked eg. kernel => linux", func() {
 			// 4 seconds because default time for goconvey
-			facts, _, err := getFacts([]string{"kernel"}, 4*time.Second)
+			facts, _, err := getFacts([]string{"kernel"}, 4*time.Second, nil)
 			So(err, ShouldBeNil)
 			So(facts, ShouldNotBeEmpty)
 			So(len(*facts), ShouldEqual, 1)
@@ -39,7 +61,7 @@ func TestGetFacts(t *testing.T) {
 
 		Convey("returns nil in fact value when for non existing fact", func() {
 			// 4 seconds because default time for goconvey
-			facts, _, err := getFacts([]string{"thereisnosuchfact"}, 4*time.Second)
+			facts, _, err := getFacts([]string{"thereisnosuchfact"}, 4*time.Second, nil)
 			So(err, ShouldBeNil)
 			So(facts, ShouldNotBeEmpty)
 			So(len(*facts), ShouldEqual, 1)
