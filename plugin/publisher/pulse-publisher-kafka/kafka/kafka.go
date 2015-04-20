1527961b838a32406bd9b4f1a863f3d3febc3667
Nicholas Weaver <lynxbat@gmail.com>
2015-04-20 13:00:22 -0700

    Cleanup and testing * added script for docker machine based int testing (manual) * added script to confirm production of messages and consumption

--- plugin/publisher/pulse-publisher-kafka/kafka/kafka.go
+++ plugin/publisher/pulse-publisher-kafka/kafka/kafka.go
@@ -36,23 +36,11 @@ func ConfigPolicyNode() *cpolicy.ConfigPolicyNode {
 	return config
 }
 
-// func (rmq *rmqPublisher) Publish(data []byte) error {
-// 	err := publishDataToRmq(data, rmq.rmqAddress, rmq.rmqExName, rmq.rmqRtKey, rmq.rmqExKind)
-// 	return err
-// }
-
 type Kafka struct{}
 
 func NewKafkaPublisher() *Kafka {
 	var k *Kafka
-	//TODO get data from config
-	// rmqpub := new(rmqPublisher)
-	// rmqpub.rmqAddress = defaultRmqAddress
-	// rmqpub.rmqExName = defaultExchangeName
-	// rmqpub.rmqExKind = defaultExchangeKind
-	// rmqpub.rmqRtKey = defaultRoutingKey
 	return k
-
 }
 
 // Publish sends data to a Kafka server
@@ -66,11 +54,6 @@ func (k *Kafka) Publish(contentType string, content []byte, config map[string]ct
 
 // Internal method after data has been converted to serialized bytes to send
 func (k *Kafka) publish(topic string, brokers []string, content []byte) {
-	// handle config errors
-	// ! these should not happen if the config rule is created correctly
-
-	fmt.Println(brokers)
-
 	producer, err := sarama.NewSyncProducer(brokers, nil)
 	if err != nil {
 		fmt.Println("ERROR:", err)
@@ -92,56 +75,3 @@ func handleErr(e error) {
 		panic(e)
 	}
 }
-
-// type rmqPublisher struct {
-// rmqAddress string
-// rmqExName  string
-// rmqExKind  string
-// rmqRtKey   string
-// }
-
-// const (
-// 	name       = "rabbitmq"
-// 	version    = 1
-// 	pluginType = plugin.PublisherPluginType
-
-// 	defaultRmqAddress   = "127.0.0.1:5672"
-// 	defaultExchangeName = "pulse"
-// 	defaultExchangeKind = "fanout"
-// 	defaultRoutingKey   = "metrics"
-// )
-
-// func publishDataToRmq(data []byte, address string, exName string, rtKey string, exKind string) error {
-// 	conn, err := amqp.Dial("amqp://" + address)
-// 	if err != nil {
-// 		log.Printf("RMQ Publisher: cannot open connection, %s", err)
-// 		return err
-// 	}
-// 	defer conn.Close()
-
-// 	c, err := conn.Channel()
-// 	if err != nil {
-// 		log.Printf("RMQ Publisher: cannot open channel: %s", err)
-// 		return err
-// 	}
-
-// 	err = c.ExchangeDeclare(exName, exKind, true, false, false, false, nil)
-// 	if err != nil {
-// 		log.Printf("RMQ Publisher: cannot declare exchange: %v", err)
-// 	}
-
-// 	msg := amqp.Publishing{
-// 		DeliveryMode: amqp.Persistent,
-// 		Timestamp:    time.Now(),
-// 		ContentType:  "text/plain",
-// 		Body:         data,
-// 	}
-
-// 	err = c.Publish(exName, rtKey, false, false, msg)
-// 	if err != nil {
-// 		log.Printf("RMQ Publisher: cannot publish metric %v", err)
-// 		return err
-// 	}
-
-// 	return nil
-// }
--- plugin/publisher/pulse-publisher-kafka/kafka/kafka_integration_test.go
+++ plugin/publisher/pulse-publisher-kafka/kafka/kafka_integration_test.go
@@ -1,10 +1,9 @@
-//
-// +bbbuild integration
-
 package kafka
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
@@ -13,25 +12,92 @@ import (
 	"github.com/intelsdilabs/pulse/core/ctypes"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"gopkg.in/Shopify/sarama.v1"
 )
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 // integration test
 func TestPublish(t *testing.T) {
+	// This integration test requires KAFKA_BROKERS
+	if os.Getenv("PULSE_TEST_KAFKA") == "" {
+		fmt.Println("Skipping integration")
+		return
+	}
+	brokers := os.Getenv("PULSE_TEST_KAFKA")
+
+	// Pick a random topic
+	topic := fmt.Sprintf("%d", time.Now().Nanosecond())
+	fmt.Printf("Topic: %s\n", topic)
+
 	Convey("Publish to Kafka", t, func() {
 		k := NewKafkaPublisher()
 
 		// Build some config
 		cdn := cdata.NewNode()
-		cdn.AddItem("brokers", ctypes.ConfigValueStr{"172.16.125.132:9092"})
-		cdn.AddItem("topic", ctypes.ConfigValueStr{"test"})
+		cdn.AddItem("brokers", ctypes.ConfigValueStr{brokers})
+		cdn.AddItem("topic", ctypes.ConfigValueStr{topic})
 
 		//
 		p := ConfigPolicyNode()
 		f, err := p.Process(cdn.Table())
 		So(getProcessErrorStr(err), ShouldEqual, "")
 
-		k.Publish("", []byte(time.Now().String()), *f)
+		t := time.Now().String()
+
+		// Setup a consumer to wait
+
+		// Send data to create topic. There is a weird bug where first message won't be consumed
+		// ref: http://mail-archives.apache.org/mod_mbox/kafka-users/201411.mbox/%3CCAHwHRrVmwyJg-1eyULkzwCUOXALuRA6BqcDV-ffSjEQ+tmT7dw@mail.gmail.com%3E
+		k.Publish("", []byte(t), *f)
+		// Send the same message. This will be consumed.
+		k.Publish("", []byte(t), *f)
+
+		// start timer and wait
+		m, mErr := consumer(topic, brokers)
+		So(mErr, ShouldBeNil)
+		So(m, ShouldNotBeNil)
+		So(string(m.Value), ShouldEqual, t)
 	})
+}
+
+func consumer(topic, brokers string) (*sarama.ConsumerMessage, error) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true // Handle errors manually instead of letting Sarama log them.
+
+	master, err := sarama.NewConsumer([]string{brokers}, config)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		master.Close()
+	}()
+
+	consumer, err := master.ConsumePartition(topic, 0, 0)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		consumer.Close()
+	}()
+
+	msgCount := 0
+	timeout := make(chan bool, 1)
+
+	go func() {
+		time.Sleep(time.Millisecond * 500)
+		timeout <- true
+	}()
+
+	select {
+	case err := <-consumer.Errors():
+		return nil, err
+	case m := <-consumer.Messages():
+		msgCount++
+		return m, nil
+	case <-timeout:
+		return nil, errors.New("timed out waiting for produced message")
+	}
 }
 
 func getProcessErrorStr(err *cpolicy.ProcessingErrors) string {
