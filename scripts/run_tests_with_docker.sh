d229b302da7b8c63e05ca39ffa3fd351b119f1b1
Joel Cooklin <joel.cooklin@gmail.com>
2015-05-20 12:22:36 -0700

    Reduces image layers

--- scripts/run_tests_with_docker.sh
+++ scripts/run_tests_with_docker.sh
@@ -5,9 +5,15 @@ die() {
     exit 1
 }
 
-[ $# -eq 1 ] || die "arg missing: github token is required so we can clone a private repo)"
+if [ $# -eq  2 ]; then
+	GIT_TOKEN=$1
+fi
 
-sed s/\<GIT_TOKEN\>/$1/ scripts/Dockerfile > scripts/Dockerfile.tmp
+if [ -z "${GIT_TOKEN}" ]; then
+	die "arg missing: github token is required so we can clone a private repo)"
+fi
+
+sed s/\<GIT_TOKEN\>/${GIT_TOKEN}/ scripts/Dockerfile > scripts/Dockerfile.tmp
 docker build -t sdilabs-x/pulse-test -f scripts/Dockerfile.tmp .
 rm scripts/Dockerfile.tmp
 docker run -it sdilabs-x/pulse-test scripts/test.sh
