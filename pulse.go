000da53620aa0370f3db19c885136a75765db92c
Nicholas Weaver <lynxbat@gmail.com>
2015-06-11 12:20:35 -0700

    Switch to github.com/Sirupsen/logrus for logging * This includes pulse.go changes first

--- pulse.go
+++ pulse.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	// "log"
+	log "github.com/Sirupsen/logrus"
 	"os"
 	"os/signal"
 	"runtime"
@@ -11,7 +11,7 @@ import (
 
 	"github.com/intelsdi-x/pulse/control"
 	"github.com/intelsdi-x/pulse/mgmt/rest"
-	"github.com/intelsdi-x/pulse/pkg/logger"
+	// "github.com/intelsdi-x/pulse/pkg/logger"
 	"github.com/intelsdi-x/pulse/scheduler"
 )
 
@@ -34,6 +34,7 @@ const (
 type coreModule interface {
 	Start() error
 	Stop()
+	Name() string
 }
 
 func main() {
@@ -44,37 +45,58 @@ func main() {
 	}
 
 	if *logLevel < 1 || *logLevel > 5 {
-		logger.Fatal("main", fmt.Sprintf("log level is invalid: was %d, valid (1-5)", *logLevel))
+		log.WithFields(
+			log.Fields{
+				"block": "main",
+				"level": *logLevel,
+			}).Fatal("log level was invalid (needs: 1-5)")
 		os.Exit(1)
 	}
 
-	logger.SetLevel(logger.LogLevel(*logLevel))
+	log.SetLevel(getLevel(*logLevel))
 
 	if *logPath != "" {
 
 		f, err := os.Stat(*logPath)
 		if err != nil {
 
-			logger.Error("main", fmt.Sprintf("bad log path(%s) - %s\n", *logPath, err.Error()))
+			log.WithFields(
+				log.Fields{
+					"block":   "main",
+					"error":   err.Error(),
+					"logpath": *logPath,
+				}).Fatal("bad log path (must be a dir)")
 			os.Exit(0)
 		}
 		if !f.IsDir() {
-			logger.Error("main", fmt.Sprintf("bad log path(%s) - not a directory\n", *logPath))
+			log.WithFields(
+				log.Fields{
+					"block":   "main",
+					"logpath": *logPath,
+				}).Fatal("bad log path this is not a directory")
 			os.Exit(0)
 		}
 
 		file, err2 := os.OpenFile(fmt.Sprintf("%s/pulse.log", *logPath), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err2 != nil {
-			logger.Error("main", fmt.Sprintf("bad log path(%s) - %s\n", *logPath, err.Error()))
+			log.WithFields(
+				log.Fields{
+					"block":   "main",
+					"error":   err2.Error(),
+					"logpath": *logPath,
+				}).Fatal("bad log path")
 		}
 		defer file.Close()
-		logger.Output = file
+		log.SetOutput(file)
 	}
 
 	// Set Max Processors for the Pulse agent.
 	setMaxProcs()
 
-	logger.Info("main", "pulse agent starting")
+	log.WithFields(
+		log.Fields{
+			"block": "main",
+		}).Info("pulse agent starting")
 	c := control.New()
 	s := scheduler.New(
 		scheduler.CollectQSizeOption(defaultQueueSize),
@@ -89,14 +111,14 @@ func main() {
 	startInterruptHandling(c, s)
 
 	//  Start our modules
-	if err := startModule("plugin controller", c); err != nil {
-		printErrorAndExit("plugin controller", err)
+	if err := startModule(c); err != nil {
+		printErrorAndExit(c.Name(), err)
 	}
-	if err := startModule("scheduler", s); err != nil {
+	if err := startModule(s); err != nil {
 		if c.Started {
 			c.Stop()
 		}
-		printErrorAndExit("scheduler", err)
+		printErrorAndExit(s.Name(), err)
 	}
 
 	// init rest mgmt interface
@@ -119,43 +141,76 @@ func setMaxProcs() {
 		_maxProcs = envGoMaxProcs
 	} else if envGoMaxProcs > numProcs {
 		// We do not allow the user to exceed the number of cores in the system.
-		logger.Warning("main", "ENV variable GOMAXPROCS greater than number of cores in the system.")
-		logger.Info("main", "setting pulse to use the number of cores in the system.")
+		log.WithFields(
+			log.Fields{
+				"block":    "main",
+				"maxprocs": _maxProcs,
+			}).Warning("ENV variable GOMAXPROCS greater than number of cores in the system")
+		log.WithFields(
+			log.Fields{
+				"block":    "main",
+				"maxprocs": _maxProcs,
+			}).Error("setting pulse to use the number of cores in the system")
 		_maxProcs = numProcs
 	} else if *maxProcs > 0 && *maxProcs <= numProcs {
 		// Our flag override is set. Use this value
 		_maxProcs = *maxProcs
 	} else if *maxProcs > numProcs {
 		// Do not let the user set a value larger than number of cores in the system
-		logger.Warning("main", "flag max_procs exceeds number of cores in the system. Setting Pulse to use the number of cores in the system")
+		log.WithFields(
+			log.Fields{
+				"block":    "main",
+				"maxprocs": _maxProcs,
+			}).Warning("flag max_procs exceeds number of cores in the system. Setting Pulse to use the number of cores in the system")
 		_maxProcs = numProcs
 	} else if *maxProcs < 0 {
 		// Do not let the user set a negative value to get around number of cores limit
-		logger.Warning("main", "flag max_procs set to negative number. Setting Pulse to use 1 core.")
+		log.WithFields(
+			log.Fields{
+				"block":    "main",
+				"maxprocs": _maxProcs,
+			}).Warning("flag max_procs set to negative number. Setting Pulse to use 1 core")
 		_maxProcs = 1
 	}
 
-	logger.Debug("main", fmt.Sprintf("GOMAXPROCS=%v\n", _maxProcs))
+	log.WithFields(
+		log.Fields{
+			"block":    "main",
+			"maxprocs": _maxProcs,
+		}).Debug("maxprocs")
 	runtime.GOMAXPROCS(_maxProcs)
 
 	//Verify setting worked
 	actualNumProcs := runtime.GOMAXPROCS(0)
 	if actualNumProcs != _maxProcs {
-		logger.Warning("main", fmt.Sprintf("specified max procs of %v but using %v", _maxProcs, actualNumProcs))
+		log.WithFields(
+			log.Fields{
+				"block":          "main",
+				"given maxprocs": _maxProcs,
+				"real maxprocs":  actualNumProcs,
+			}).Warning("not using given maxprocs")
 	}
 }
 
-func startModule(name string, m coreModule) error {
+func startModule(m coreModule) error {
 	err := m.Start()
 	if err == nil {
-		logger.Info("main", fmt.Sprintf("%s module started\n", name))
+		log.WithFields(
+			log.Fields{
+				"block":  "startModule",
+				"module": m.Name(),
+			}).Info("module started")
 	}
 	return err
 }
 
 func printErrorAndExit(name string, err error) {
-	logger.Error("main", err.Error())
-	logger.Error("main", fmt.Sprintf("error starting pulse agent %s module. Exiting now.", name))
+	log.WithFields(
+		log.Fields{
+			"block":  "main",
+			"error":  err.Error(),
+			"module": name,
+		}).Fatal("error starting module")
 	os.Exit(1)
 }
 
@@ -166,11 +221,41 @@ func startInterruptHandling(modules ...coreModule) {
 	//Let's block until someone tells us to quit
 	go func() {
 		sig := <-c
-		logger.Info("main", "stopping pulse agent modules")
+		log.WithFields(
+			log.Fields{
+				"block": "main",
+			}).Info("shutting down modules")
+
 		for _, m := range modules {
+			log.WithFields(
+				log.Fields{
+					"block":  "main",
+					"module": m.Name(),
+				}).Info("stopping module")
 			m.Stop()
 		}
-		logger.Info("main", fmt.Sprintf("exiting given signal: %v", sig))
+		log.WithFields(
+			log.Fields{
+				"block":  "main",
+				"signal": sig.String(),
+			}).Info("exiting on signal")
 		os.Exit(0)
 	}()
+}
+
+func getLevel(i int) log.Level {
+	switch i {
+	case 1:
+		return log.DebugLevel
+	case 2:
+		return log.InfoLevel
+	case 3:
+		return log.WarnLevel
+	case 4:
+		return log.ErrorLevel
+	case 5:
+		return log.FatalLevel
+	default:
+		panic("bad level")
+	}
 }
