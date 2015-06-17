d5542059faea7006a18c80c11f81c90cf6fe702d
Nicholas Weaver <lynxbat@gmail.com>
2015-06-16 17:48:18 -0700

    Adds core/perror * perror is a package for a pulse specific error interface (PulseError) that allows attachement of fields following the pattern from logrus * This allows fields that are important to the error to be passed up

---
+++ core/perror/perror.go
@@ -0,0 +1,37 @@
+package perror
+
+type PulseError interface {
+	error
+	Fields() map[string]interface{}
+}
+
+type pulseError struct {
+	err    error
+	fields map[string]interface{}
+}
+
+func New(e error) *pulseError {
+	// Catch someone trying to wrap a pe around a pe.
+	// We throw a panic to make them fix this.
+	switch e.(type) {
+	case PulseError:
+		panic("You are trying to wrap a pulseError around a PulseError. Don't do this.")
+	}
+	return &pulseError{err: e, fields: make(map[string]interface{})}
+}
+
+func (p *pulseError) SetFields(f map[string]interface{}) {
+	p.fields = f
+}
+
+func (p *pulseError) Fields() map[string]interface{} {
+	return p.fields
+}
+
+func (p *pulseError) Error() string {
+	return p.err.Error()
+}
+
+func (p *pulseError) String() string {
+	return p.Error()
+}
