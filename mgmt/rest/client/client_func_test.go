db48551760087de67f77d7ef73a04b9dbfeacd9b
Nicholas Weaver <lynxbat@gmail.com>
2015-06-28 10:59:32 -0700

    Coverage and slight refactor for client plugin calls

--- mgmt/rest/client/client_func_test.go
+++ mgmt/rest/client/client_func_test.go
@@ -4,6 +4,7 @@ package client
 
 import (
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
@@ -16,6 +17,11 @@ import (
 )
 
 var (
+	PULSE_PATH          = os.Getenv("PULSE_PATH")
+	DUMMY_PLUGIN_PATH1  = PULSE_PATH + "/plugin/collector/pulse-collector-dummy1"
+	DUMMY_PLUGIN_PATH2  = PULSE_PATH + "/plugin/collector/pulse-collector-dummy2"
+	RIEMANN_PLUGIN_PATH = PULSE_PATH + "/plugin/publisher/pulse-publisher-riemann"
+
 	NextPort = 9000
 )
 
@@ -30,7 +36,7 @@ func incrPort() {
 
 // REST API instances that are started are killed when the tests end.
 // When we eventually have a REST API Stop command this can be killed.
-func startAPI(port int) {
+func startAPI(port int) string {
 	// Start a REST API to talk to
 	log.SetLevel(log.FatalLevel)
 	r := rest.New()
@@ -43,9 +49,142 @@ func startAPI(port int) {
 	r.BindTaskManager(s)
 	r.Start(":" + fmt.Sprint(port))
 	time.Sleep(time.Millisecond * 100)
+	return fmt.Sprintf("http://localhost:%d", port)
 }
 
 func TestPulseClient(t *testing.T) {
 	Convey("REST API functional V1", t, func() {
+		Convey("GetPlugins", func() {
+			Convey("empty list", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				p := c.GetPlugins(false)
+
+				So(p.Err, ShouldBeNil)
+				So(len(p.LoadedPlugins), ShouldEqual, 0)
+				So(p.AvailablePlugins, ShouldBeEmpty)
+			})
+			Convey("single item", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				p := c.GetPlugins(false)
+
+				So(p.Err, ShouldBeNil)
+				So(len(p.LoadedPlugins), ShouldEqual, 1)
+				So(p.AvailablePlugins, ShouldBeEmpty)
+			})
+			Convey("multiple items", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				c.LoadPlugin(DUMMY_PLUGIN_PATH2)
+				p := c.GetPlugins(false)
+
+				So(p.Err, ShouldBeNil)
+				So(len(p.LoadedPlugins), ShouldEqual, 2)
+				So(p.AvailablePlugins, ShouldBeEmpty)
+			})
+		})
+		Convey("LoadPlugin", func() {
+			Convey("single load", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				p := c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				So(p.Err, ShouldBeNil)
+				So(p.LoadedPlugins, ShouldNotBeEmpty)
+				So(p.LoadedPlugins[0].Name, ShouldEqual, "dummy1")
+				So(p.LoadedPlugins[0].Version, ShouldEqual, 1)
+				So(p.LoadedPlugins[0].LoadedTime().Unix(), ShouldBeLessThanOrEqualTo, time.Now().Unix())
+			})
+			Convey("multiple load", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				p1 := c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				So(p1.Err, ShouldBeNil)
+				So(p1.LoadedPlugins, ShouldNotBeEmpty)
+				So(p1.LoadedPlugins[0].Name, ShouldEqual, "dummy1")
+				So(p1.LoadedPlugins[0].Version, ShouldEqual, 1)
+				So(p1.LoadedPlugins[0].LoadedTime().Unix(), ShouldBeLessThanOrEqualTo, time.Now().Unix())
+
+				p2 := c.LoadPlugin(DUMMY_PLUGIN_PATH2)
+				So(p2.Err, ShouldBeNil)
+				So(p2.LoadedPlugins, ShouldNotBeEmpty)
+				So(p2.LoadedPlugins[0].Name, ShouldEqual, "dummy2")
+				So(p2.LoadedPlugins[0].Version, ShouldEqual, 2)
+				So(p2.LoadedPlugins[0].LoadedTime().Unix(), ShouldBeLessThanOrEqualTo, time.Now().Unix())
+			})
+
+			Convey("already loaded", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				p1 := c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				So(p1.Err, ShouldBeNil)
+				So(p1.LoadedPlugins, ShouldNotBeEmpty)
+				So(p1.LoadedPlugins[0].Name, ShouldEqual, "dummy1")
+				So(p1.LoadedPlugins[0].Version, ShouldEqual, 1)
+				So(p1.LoadedPlugins[0].LoadedTime().Unix(), ShouldBeLessThanOrEqualTo, time.Now().Unix())
+
+				p2 := c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				So(p2.Err, ShouldNotBeNil)
+				So(p2.Err.Error(), ShouldEqual, "plugin is already loaded")
+			})
+		})
+
+		Convey("UnloadPlugin", func() {
+			Convey("unload unknown plugin", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				p := c.UnloadPlugin("foo", 3)
+				So(p.Err, ShouldNotBeNil)
+				So(p.Err.Error(), ShouldEqual, "plugin not found (has it already been unloaded?)")
+			})
+
+			Convey("unload only one there is", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				p := c.UnloadPlugin("dummy1", 1)
+				So(p.Err, ShouldBeNil)
+				So(p.Name, ShouldEqual, "dummy1")
+				So(p.Version, ShouldEqual, 1)
+				So(p.Type, ShouldEqual, "collector")
+			})
+
+			Convey("unload one of multiple", func() {
+				port := getPort()
+				uri := startAPI(port)
+				c := New(uri, "v1")
+
+				c.LoadPlugin(DUMMY_PLUGIN_PATH1)
+				c.LoadPlugin(DUMMY_PLUGIN_PATH2)
+				p1 := c.UnloadPlugin("dummy2", 2)
+				So(p1.Err, ShouldBeNil)
+				So(p1.Name, ShouldEqual, "dummy2")
+				So(p1.Version, ShouldEqual, 2)
+				So(p1.Type, ShouldEqual, "collector")
+
+				p2 := c.GetPlugins(false)
+				So(p2.Err, ShouldBeNil)
+				So(len(p2.LoadedPlugins), ShouldEqual, 1)
+				So(p2.LoadedPlugins[0].Name, ShouldEqual, "dummy1")
+			})
+		})
 	})
 }
--- mgmt/rest/client/plugin.go
+++ mgmt/rest/client/plugin.go
@@ -8,40 +8,28 @@ import (
 	"github.com/intelsdi-x/pulse/mgmt/rest/rbody"
 )
 
-type Plugin struct {
-	Name            string     `json:"name"`
-	Version         int        `json:"version,omitempty"`
-	TypeName        string     `json:"type"`
-	Status          string     `json:"status,omitempty"`
-	LoadedTimestamp *time.Time `json:"loaded_timestamp,omitempty"`
-	HitCount        int        `json:"hit_count,omitempty"`
-	LastHit         *time.Time `json:"last_hit,omitempty"`
-}
-
-// TODO, this should RETURN the plugin that was loaded...
-func (c *Client) LoadPlugin(p string) error {
+func (c *Client) LoadPlugin(p string) *LoadPluginResult {
+	r := new(LoadPluginResult)
 	resp, err := c.pluginUploadRequest(p)
 	if err != nil {
-		return err
+		r.Err = err
+		return r
 	}
 
 	switch resp.Meta.Type {
-	// TODO change this to concrete const type when Joel adds it
-	case "plugin_load":
-		//
+	case rbody.PluginsLoadedType:
+		pl := resp.Body.(*rbody.PluginsLoaded)
+		r.LoadedPlugins = convertLoadedPlugins(pl.LoadedPlugins)
 	case rbody.ErrorType:
-		return resp.Body.(*rbody.Error)
+		r.Err = resp.Body.(*rbody.Error)
 	default:
-		return ErrAPIResponseMetaType
+		r.Err = ErrAPIResponseMetaType
 	}
-	return ErrUnknown
+	return r
 }
 
 func (c *Client) UnloadPlugin(name string, version int) *UnloadPluginResult {
-	r := &UnloadPluginResult{
-		Name:    name,
-		Version: version,
-	}
+	r := &UnloadPluginResult{}
 	resp, err := c.do("DELETE", fmt.Sprintf("/plugins/%s/%d", url.QueryEscape(name), version), ContentTypeJSON)
 	if err != nil {
 		r.Err = err
@@ -49,9 +37,10 @@ func (c *Client) UnloadPlugin(name string, version int) *UnloadPluginResult {
 	}
 
 	switch resp.Meta.Type {
-	// TODO change this to concrete const type when Joel adds it
 	case rbody.PluginUnloadedType:
 		// Success
+		up := resp.Body.(*rbody.PluginUnloaded)
+		r = &UnloadPluginResult{up, nil}
 	case rbody.ErrorType:
 		r.Err = resp.Body.(*rbody.Error)
 	default:
@@ -81,8 +70,8 @@ func (c *Client) GetPlugins(details bool) *GetPluginsResult {
 	case rbody.PluginListReturnedType:
 		// Success
 		b := resp.Body.(*rbody.PluginListReturned)
-		r.LoadedPlugins = b.LoadedPlugins
-		r.AvailablePlugins = b.AvailablePlugins
+		r.LoadedPlugins = convertLoadedPlugins(b.LoadedPlugins)
+		r.AvailablePlugins = convertAvailablePlugins(b.AvailablePlugins)
 		return r
 	case rbody.ErrorType:
 		r.Err = resp.Body.(*rbody.Error)
@@ -93,14 +82,47 @@ func (c *Client) GetPlugins(details bool) *GetPluginsResult {
 }
 
 type GetPluginsResult struct {
-	LoadedPlugins    []rbody.LoadedPlugin
-	AvailablePlugins []rbody.AvailablePlugin
+	LoadedPlugins    []LoadedPlugin
+	AvailablePlugins []AvailablePlugin
 	Err              error
+}
+
+type LoadPluginResult struct {
+	LoadedPlugins []LoadedPlugin
+	Err           error
 }
 
 // UnloadPluginResponse is the response from pulse/client on an UnloadPlugin call.
 type UnloadPluginResult struct {
-	Name    string
-	Version int
-	Err     error
+	*rbody.PluginUnloaded
+	Err error
+}
+
+// We wrap this so we can provide some functionality (like LoadedTime)
+type LoadedPlugin struct {
+	*rbody.LoadedPlugin
+}
+
+func (l *LoadedPlugin) LoadedTime() time.Time {
+	return time.Unix(l.LoadedTimestamp, 0)
+}
+
+type AvailablePlugin struct {
+	*rbody.AvailablePlugin
+}
+
+func convertLoadedPlugins(r []rbody.LoadedPlugin) []LoadedPlugin {
+	lp := make([]LoadedPlugin, len(r))
+	for i, _ := range r {
+		lp[i] = LoadedPlugin{&r[i]}
+	}
+	return lp
+}
+
+func convertAvailablePlugins(r []rbody.AvailablePlugin) []AvailablePlugin {
+	lp := make([]AvailablePlugin, len(r))
+	for i, _ := range r {
+		lp[i] = AvailablePlugin{&r[i]}
+	}
+	return lp
 }
