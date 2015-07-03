5096cc5d99ea6b9126d76dc1161b9b190d4c7a00
Nicholas Weaver <lynxbat@gmail.com>
2015-07-03 13:33:02 -0700

    Watcher handling of task stop, start, disabled states

--- scheduler/watcher.go
+++ scheduler/watcher.go
@@ -8,6 +8,10 @@ import (
 	"github.com/intelsdi-x/pulse/core"
 )
 
+var (
+	watcherLog = log.WithField("_module", "scheduler-watcher")
+)
+
 type TaskWatcher struct {
 	id      uint64
 	taskIds []uint64
@@ -45,6 +49,10 @@ func (t *taskWatcherCollection) rm(taskId uint64, tw *TaskWatcher) {
 	if t.coll[taskId] != nil {
 		for i, w := range t.coll[taskId] {
 			if w == tw {
+				watcherLog.WithFields(log.Fields{
+					"task-id":         taskId,
+					"task-watcher-id": tw.id,
+				}).Debug("removing watch from task")
 				t.coll[taskId] = append(t.coll[taskId][:i], t.coll[taskId][i+1:]...)
 				if len(t.coll[taskId]) == 0 {
 					delete(t.coll, taskId)
@@ -75,7 +83,7 @@ func (t *taskWatcherCollection) add(taskId uint64, twh core.TaskWatcherHandler) 
 	tw.taskIds = append(tw.taskIds, taskId)
 	// Add this task watcher in
 	t.coll[taskId] = append(t.coll[taskId], tw)
-	log.WithFields(log.Fields{
+	watcherLog.WithFields(log.Fields{
 		"task-id":         taskId,
 		"task-watcher-id": tw.id,
 	}).Debug("Added to task watcher collection")
@@ -86,21 +94,90 @@ func (t *taskWatcherCollection) handleMetricCollected(taskId uint64, m []core.Me
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 	// no taskID means no watches, early exit
-	if t.coll[taskId] == nil {
-		log.WithFields(log.Fields{
-			"task-id": taskId,
-		}).Debug("No tack watchers")
+	if t.coll[taskId] == nil || len(t.coll[taskId]) == 0 {
+		// Uncomment this debug line if needed. Otherwise this is too verbose for even debug level.
+		// watcherLog.WithFields(log.Fields{
+		// 	"task-id": taskId,
+		// }).Debug("no watchers")
 		return
 	}
 	// Walk all watchers for a task ID
 	for i, v := range t.coll[taskId] {
 		// Check if they have a catcher assigned
-		log.WithFields(log.Fields{
+		watcherLog.WithFields(log.Fields{
 			"task-id":         taskId,
 			"task-watcher-id": i,
 		}).Debug("calling taskwatcher collection func")
 		// Call the catcher
 		v.handler.CatchCollection(m)
 	}
+}
 
+func (t *taskWatcherCollection) handleTaskStarted(taskId uint64) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	// no taskID means no watches, early exit
+	if t.coll[taskId] == nil || len(t.coll[taskId]) == 0 {
+		// Uncomment this debug line if needed. Otherwise this is too verbose for even debug level.
+		watcherLog.WithFields(log.Fields{
+			"task-id": taskId,
+		}).Debug("no watchers")
+		return
+	}
+	// Walk all watchers for a task ID
+	for i, v := range t.coll[taskId] {
+		// Check if they have a catcher assigned
+		watcherLog.WithFields(log.Fields{
+			"task-id":         taskId,
+			"task-watcher-id": i,
+		}).Debug("calling taskwatcher task started func")
+		// Call the catcher
+		v.handler.CatchTaskStarted()
+	}
+}
+
+func (t *taskWatcherCollection) handleTaskStopped(taskId uint64) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	// no taskID means no watches, early exit
+	if t.coll[taskId] == nil || len(t.coll[taskId]) == 0 {
+		// Uncomment this debug line if needed. Otherwise this is too verbose for even debug level.
+		watcherLog.WithFields(log.Fields{
+			"task-id": taskId,
+		}).Debug("no watchers")
+		return
+	}
+	// Walk all watchers for a task ID
+	for i, v := range t.coll[taskId] {
+		// Check if they have a catcher assigned
+		watcherLog.WithFields(log.Fields{
+			"task-id":         taskId,
+			"task-watcher-id": i,
+		}).Debug("calling taskwatcher task stopped func")
+		// Call the catcher
+		v.handler.CatchTaskStopped()
+	}
+}
+
+func (t *taskWatcherCollection) handleTaskDisabled(taskId uint64, why string) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	// no taskID means no watches, early exit
+	if t.coll[taskId] == nil || len(t.coll[taskId]) == 0 {
+		// Uncomment this debug line if needed. Otherwise this is too verbose for even debug level.
+		// watcherLog.WithFields(log.Fields{
+		// 	"task-id": taskId,
+		// }).Debug("no watchers")
+		return
+	}
+	// Walk all watchers for a task ID
+	for i, v := range t.coll[taskId] {
+		// Check if they have a catcher assigned
+		watcherLog.WithFields(log.Fields{
+			"task-id":         taskId,
+			"task-watcher-id": i,
+		}).Debug("calling taskwatcher task disabled func")
+		// Call the catcher
+		v.handler.CatchTaskDisabled(why)
+	}
 }
