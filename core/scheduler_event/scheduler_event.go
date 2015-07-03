46f5fbac5d1aeb5d032a2718b72c0658bc2904ea
Nicholas Weaver <lynxbat@gmail.com>
2015-07-03 13:32:16 -0700

    New scheduler events for task state changes

--- core/scheduler_event/scheduler_event.go
+++ core/scheduler_event/scheduler_event.go
@@ -5,9 +5,37 @@ import (
 )
 
 const (
+	TaskStarted            = "Control.TaskStarted"
+	TaskStopped            = "Control.TaskStopped"
+	TaskDisabled           = "Control.TaskDisabled"
 	MetricCollected        = "Control.MetricsCollected"
 	MetricCollectionFailed = "Control.MetricCollectionFailed"
 )
+
+type TaskStartedEvent struct {
+	TaskID uint64
+}
+
+func (e TaskStartedEvent) Namespace() string {
+	return TaskStarted
+}
+
+type TaskStoppedEvent struct {
+	TaskID uint64
+}
+
+func (e TaskStoppedEvent) Namespace() string {
+	return TaskStopped
+}
+
+type TaskDisabledEvent struct {
+	TaskID uint64
+	Why    string
+}
+
+func (e TaskDisabledEvent) Namespace() string {
+	return TaskDisabled
+}
 
 type MetricCollectedEvent struct {
 	TaskID  uint64
