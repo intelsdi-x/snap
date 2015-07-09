38a826e482ff7e47d871ad5a320869dd7b27df4c
Joel Cooklin <joel.cooklin@gmail.com>
2015-07-09 12:46:56 -0700

    Updates influxdb to 0.9.1
    - removes conversin in type switch since it's no longer needed

--- plugin/publisher/pulse-publisher-influxdb/influx/influx.go
+++ plugin/publisher/pulse-publisher-influxdb/influx/influx.go
@@ -110,17 +110,10 @@ func (f *influxPublisher) Publish(contentType string, content []byte, config map
 
 	pts := make([]client.Point, len(metrics))
 	for i, m := range metrics {
-		var v interface{}
-		switch m.Data().(type) {
-		case uint64:
-			v = int64(m.Data().(uint64))
-		default:
-			v = m.Data()
-		}
 		pts[i] = client.Point{
 			Measurement: strings.Join(m.Namespace(), "/"),
 			Fields: map[string]interface{}{
-				"value": v,
+				"value": m.Data(),
 			},
 		}
 	}
