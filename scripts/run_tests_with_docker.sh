db16cf5529e26c7f64db78159e155d2dddbf93d8
Tiffany Jernigan <tiffany.jernigan@intel.com>
2015-11-13 21:59:01 -0800

    Switched sdilabs-x to intelsdi-x in docker run script

--- scripts/run_tests_with_docker.sh
+++ scripts/run_tests_with_docker.sh
@@ -31,6 +31,6 @@ if [ -z "${GIT_TOKEN}" ]; then
 fi
 
 sed s/\<GIT_TOKEN\>/${GIT_TOKEN}/ scripts/Dockerfile > scripts/Dockerfile.tmp
-docker build -t sdilabs-x/pulse-test -f scripts/Dockerfile.tmp .
+docker build -t intelsdi-x/pulse-test -f scripts/Dockerfile.tmp .
 rm scripts/Dockerfile.tmp
-docker run -it sdilabs-x/pulse-test scripts/test.sh
+docker run -it intelsdi-x/pulse-test scripts/test.sh
