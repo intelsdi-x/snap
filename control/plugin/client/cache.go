200e817fac20d71cf3aa31008b6a7170409722ac
Niklas Q. Nielsen <nik@qni.dk>
2015-11-17 10:16:16 -0800

    Reintroduce deleted license lines

--- control/plugin/client/cache.go
+++ control/plugin/client/cache.go
@@ -5,6 +5,8 @@ http://www.apache.org/licenses/LICENSE-2.0.txt
 Copyright 2015 Intel Corporation
 
 Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
