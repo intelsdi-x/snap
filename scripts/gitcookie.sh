dc12496b17d94763fcf75690f8547cba09f53383
geauxvirtual <justin.guidroz@gmail.com>
2016-03-21 10:56:23 -0700

    Fix #787: Set gitcookie for go.googlesource.com due to rate limit for dependency

---
+++ scripts/gitcookie.sh
@@ -0,0 +1,11 @@
+touch ~/.gitcookies
+chmod 0600 ~/.gitcookies
+
+git config --global http.cookiefile ~/.gitcookies
+
+tr , \\t <<\__END__ >>~/.gitcookies
+go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-snappytheturtle1202.gmail.com=1/4zg9qf0zy1bmnMpEnqT7IL-rcL5_y8XP7bpfYE5DzN0
+go-review.googlesource.com,FALSE,/,TRUE,2147483647,o,git-snappytheturtle1202.gmail.com=1/4zg9qf0zy1bmnMpEnqT7IL-rcL5_y8XP7bpfYE5DzN0
+__END__
+
+
