f786aeeee4ede8e7bf10cb9d95b71cc8c5b42df6
Cody Roseborough <Cody.Roseborough@gmail.com>
2016-06-20 15:24:46 -0700

    Adds grpc client with support for collection

--- control/plugin/client/grpc.go
+++ control/plugin/client/grpc.go
@@ -21,6 +21,11 @@ package client
 
 import (
 	"crypto/rsa"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -28,6 +33,7 @@ import (
 
 	"github.com/intelsdi-x/snap/control/plugin"
 	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/control/plugin/encrypter"
 	"github.com/intelsdi-x/snap/control/plugin/rpc"
 	"github.com/intelsdi-x/snap/core"
 	"github.com/intelsdi-x/snap/core/ctypes"
@@ -35,83 +41,267 @@ import (
 	"github.com/intelsdi-x/snap/pkg/rpcutil"
 )
 
-type callsGrpcCollector interface {
-	SetKey(context.Context, *rpc.SetKeyArg, ...grpc.CallOption) (*rpc.SetKeyReply, error)
-	Ping(context.Context, *common.Empty, ...grpc.CallOption) (*rpc.PingReply, error)
-	Kill(context.Context, *rpc.KillRequest, ...grpc.CallOption) (*rpc.KillReply, error)
-	GetConfigPolicy(context.Context, *common.Empty, ...grpc.CallOption) (*rpc.GetConfigPolicyReply, error)
-	CollectMetrics(context.Context, *rpc.CollectMetricsArg, ...grpc.CallOption) (*rpc.CollectMetricsReply, error)
-	GetMetricTypes(context.Context, *rpc.GetMetricTypesArg, ...grpc.CallOption) (*rpc.GetMetricTypesReply, error)
+type pluginClient interface {
+	SetKey(ctx context.Context, in *rpc.SetKeyArg, opts ...grpc.CallOption) (*rpc.SetKeyReply, error)
+	Ping(ctx context.Context, in *common.Empty, opts ...grpc.CallOption) (*rpc.PingReply, error)
+	Kill(ctx context.Context, in *rpc.KillRequest, opts ...grpc.CallOption) (*rpc.KillReply, error)
+	GetConfigPolicy(ctx context.Context, in *common.Empty, opts ...grpc.CallOption) (*rpc.GetConfigPolicyReply, error)
 }
 
-// Native clients use golang net/rpc for communication to a native rpc server.
 type grpcClient struct {
-	connection callsGrpcCollector //CallsRPC
+	collector rpc.CollectorClient
+	processor rpc.ProcessorClient
+	publisher rpc.PublisherClient
+	plugin    pluginClient
+
 	pluginType plugin.PluginType
-	// encoder    encoding.Encoder
-	// encrypter  *encrypter.Encrypter
+	timeout    time.Duration
+	conn       *grpc.ClientConn
+	encrypter  *encrypter.Encrypter
 }
 
 // NewCollectorGrpcClient returns a collector gRPC Client.
-func NewCollectorGrpcClient(address string, port int, pub *rsa.PublicKey, secure bool) (PluginCollectorClient, error) {
-	return newGrpcClient(address, port)
+func NewCollectorGrpcClient(address string, timeout time.Duration, pub *rsa.PublicKey, secure bool) (PluginCollectorClient, error) {
+	address, port, err := parseAddress(address)
+	if err != nil {
+		return nil, err
+	}
+	p, err := newGrpcClient(address, int(port), timeout, plugin.CollectorPluginType)
+	if err != nil {
+		return nil, err
+	}
+	if secure {
+		key, err := encrypter.GenerateKey()
+		if err != nil {
+			return nil, err
+		}
+		encrypter := encrypter.New(pub, nil)
+		encrypter.Key = key
+		p.encrypter = encrypter
+	}
+
+	return p, nil
 }
 
-// func newGrpcClient(addr string, port int, pub *rsa.PublicKey, secure bool) (*grpcClient, error) {
-func newGrpcClient(addr string, port int) (*grpcClient, error) {
+// NewProcessorGrpcClient returns a processor gRPC Client.
+func NewProcessorGrpcClient(address string, timeout time.Duration, pub *rsa.PublicKey, secure bool) (PluginProcessorClient, error) {
+	address, port, err := parseAddress(address)
+	if err != nil {
+		return nil, err
+	}
+	p, err := newGrpcClient(address, int(port), timeout, plugin.ProcessorPluginType)
+	if err != nil {
+		return nil, err
+	}
+	if secure {
+		key, err := encrypter.GenerateKey()
+		if err != nil {
+			return nil, err
+		}
+		encrypter := encrypter.New(pub, nil)
+		encrypter.Key = key
+		p.encrypter = encrypter
+	}
+
+	return p, nil
+}
+
+// NewPublisherGrpcClient returns a publisher gRPC Client.
+func NewPublisherGrpcClient(address string, timeout time.Duration, pub *rsa.PublicKey, secure bool) (PluginPublisherClient, error) {
+	address, port, err := parseAddress(address)
+	if err != nil {
+		return nil, err
+	}
+	p, err := newGrpcClient(address, int(port), timeout, plugin.PublisherPluginType)
+	if err != nil {
+		return nil, err
+	}
+	if secure {
+		key, err := encrypter.GenerateKey()
+		if err != nil {
+			return nil, err
+		}
+		encrypter := encrypter.New(pub, nil)
+		encrypter.Key = key
+		p.encrypter = encrypter
+	}
+
+	return p, nil
+}
+
+func parseAddress(address string) (string, int64, error) {
+	addr := strings.Split(address, ":")
+	if len(addr) != 2 {
+		return "", 0, fmt.Errorf("bad address")
+	}
+	address = addr[0]
+	port, err := strconv.ParseInt(addr[1], 10, 64)
+	if err != nil {
+		return "", 0, err
+	}
+	return address, port, nil
+}
+
+func newGrpcClient(addr string, port int, timeout time.Duration, typ plugin.PluginType) (*grpcClient, error) {
 	conn, err := rpcutil.GetClientConnection(addr, port)
 	if err != nil {
 		return nil, err
 	}
 	p := &grpcClient{
-		connection: rpc.NewCollectorClient(conn),
+		timeout: timeout,
+		conn:    conn,
 	}
-	// p.encoder = encoding.NewGobEncoder()
 
-	// if secure {
-	// 	key, err := encrypter.GenerateKey()
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	encrypter := encrypter.New(pub, nil)
-	// 	encrypter.Key = key
-	// 	p.encrypter = encrypter
-	// 	p.encoder.SetEncrypter(encrypter)
-	// }
+	switch typ {
+	case plugin.CollectorPluginType:
+		p.collector = rpc.NewCollectorClient(conn)
+		p.plugin = p.collector
+	case plugin.ProcessorPluginType:
+		p.processor = rpc.NewProcessorClient(conn)
+		p.plugin = p.processor
+	case plugin.PublisherPluginType:
+		p.publisher = rpc.NewPublisherClient(conn)
+		p.plugin = p.publisher
+	default:
+		return nil, errors.New(fmt.Sprintf("Invalid plugin type provided %v", typ))
+	}
 
 	return p, nil
 }
 
+func getContext(timeout time.Duration) context.Context {
+	ctxTimeout, _ := context.WithTimeout(context.Background(), timeout)
+	return ctxTimeout
+}
+
 func (g *grpcClient) Ping() error {
+	_, err := g.plugin.Ping(getContext(g.timeout), &common.Empty{})
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (g *grpcClient) SetKey() error {
+	out, err := g.encrypter.EncryptKey()
+	if err != nil {
+		return err
+	}
+	reply, err := g.plugin.SetKey(getContext(g.timeout), &rpc.SetKeyArg{Key: out})
+	if err != nil {
+		return err
+	}
+
+	if reply.Error != "" {
+		return errors.New(reply.Error)
+	}
+
 	return nil
 }
 
 func (g *grpcClient) Kill(reason string) error {
+	_, err := g.plugin.Kill(getContext(g.timeout), &rpc.KillRequest{Reason: reason})
+	g.conn.Close()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (g *grpcClient) Publish(contentType string, content []byte, config map[string]ctypes.ConfigValue) error {
+	arg := &rpc.PublishArg{
+		ContentType: contentType,
+		Content:     content,
+		Config:      common.ToConfigMap(config),
+	}
+	// return is empty so we don't need it
+	_, err := g.publisher.Publish(getContext(g.timeout), arg)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (g *grpcClient) Process(contentType string, content []byte, config map[string]ctypes.ConfigValue) (string, []byte, error) {
-	return "", nil, nil
+	arg := &rpc.ProcessArg{
+		ContentType: contentType,
+		Content:     content,
+		Config:      common.ToConfigMap(config),
+	}
+	reply, err := g.processor.Process(getContext(g.timeout), arg)
+	if err != nil {
+		return "", nil, err
+	}
+	if reply.Error != "" {
+		return "", nil, errors.New(reply.Error)
+	}
+	return reply.ContentType, reply.Content, nil
 }
 
 func (g *grpcClient) CollectMetrics(mts []core.Metric) ([]core.Metric, error) {
+	arg := &rpc.CollectMetricsArg{
+		Metrics: common.NewMetrics(mts),
+	}
+	reply, err := g.collector.CollectMetrics(getContext(g.timeout), arg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if reply.Error != "" {
+		return nil, errors.New(reply.Error)
+	}
+
+	metrics := common.ToCoreMetrics(reply.Metrics)
 	var results []core.Metric
+	// Convert it to plugin.MetricType because scheduler/job.go checks that is the type before encoding
+	// and sending to the plugin.
+	//TODO(CDR): Decide what to do here if/when content-type handling is refactored
+	for _, metric := range metrics {
+		mt := plugin.MetricType{
+			Namespace_:          metric.Namespace(),
+			LastAdvertisedTime_: metric.LastAdvertisedTime(),
+			Version_:            metric.Version(),
+			Config_:             metric.Config(),
+			Data_:               metric.Data(),
+			Tags_:               metric.Tags(),
+			Unit_:               metric.Unit(),
+			Description_:        metric.Description(),
+			Timestamp_:          metric.Timestamp(),
+		}
+		results = append(results, mt)
+	}
 	return results, nil
 }
 
 func (g *grpcClient) GetMetricTypes(config plugin.ConfigType) ([]core.Metric, error) {
-	var results []core.Metric
+	arg := &rpc.GetMetricTypesArg{
+		Config: common.ToConfigMap(config.Table()),
+	}
+	reply, err := g.collector.GetMetricTypes(getContext(g.timeout), arg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if reply.Error != "" {
+		return nil, errors.New(reply.Error)
+	}
+
+	results := common.ToCoreMetrics(reply.Metrics)
 	return results, nil
 }
 
 func (g *grpcClient) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
-	return nil, nil
+	reply, err := g.plugin.GetConfigPolicy(getContext(g.timeout), &common.Empty{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if reply.Error != "" {
+		return nil, errors.New(reply.Error)
+	}
+
+	return rpc.ToConfigPolicy(reply), nil
 }
