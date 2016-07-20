49473ae942f99a40c696f5efd5ac70037e1e9c2e
Nan Liu <nan.liu@gmail.com>
2016-07-20 16:56:19 -0700

    Fix plugin catalog unknown bug.

--- js/catalog/parsed_plugin_list.js
+++ js/catalog/parsed_plugin_list.js
@@ -2,314 +2,314 @@ myfcn(
 [
   {
     "name": "Apache",
-    "type": "collector",
-    "desription": "Collects Apache webserver mod_status metrics",
+    "type": "Collector",
+    "description": "Collects Apache webserver mod_status metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-apache"
   },
   {
     "name": "CEPH",
-    "type": "collector",
-    "desription": "Collects OpenStack CEPH module metrics",
+    "type": "Collector",
+    "description": "Collects OpenStack CEPH module metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-ceph"
   },
   {
     "name": "Cinder",
-    "type": "collector",
-    "desription": "Collects OpenStack Cinder module metrics",
+    "type": "Collector",
+    "description": "Collects OpenStack Cinder module metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-cinder"
   },
   {
     "name": "CPU",
-    "type": "collector",
-    "desription": "Collects Linux CPU metrics from /proc/stat",
+    "type": "Collector",
+    "description": "Collects Linux CPU metrics from /proc/stat",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-cpu"
   },
   {
     "name": "DBI",
-    "type": "collector",
-    "desription": "Collects SQL database metrics using GO's 'SQL' package",
+    "type": "Collector",
+    "description": "Collects SQL database metrics using GO's 'SQL' package",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-dbi"
   },
   {
     "name": "Df",
-    "type": "collector",
-    "desription": "Collects Linux disk space metrics from 'df'",
+    "type": "Collector",
+    "description": "Collects Linux disk space metrics from 'df'",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-df"
   },
   {
     "name": "Disk",
-    "type": "collector",
-    "desription": "Collects Linux disk metrics from /proc/diskstats",
+    "type": "Collector",
+    "description": "Collects Linux disk metrics from /proc/diskstats",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-disk"
   },
   {
     "name": "Docker",
-    "type": "collector",
-    "desription": "Collects Docker container runtime metrics",
+    "type": "Collector",
+    "description": "Collects Docker container runtime metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-docker"
   },
   {
     "name": "Elasticsearch",
-    "type": "collector",
-    "desription": "Collects Elasticsearch cluster and nodes statistics",
+    "type": "Collector",
+    "description": "Collects Elasticsearch cluster and nodes statistics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-elasticsearch"
   },
   {
     "name": "Etcd",
-    "type": "collector",
-    "desription": "Collects etcd metrics from /metrics endpoint",
+    "type": "Collector",
+    "description": "Collects etcd metrics from /metrics endpoint",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-etcd"
   },
   {
     "name": "Ethtool",
-    "type": "collector",
-    "desription": "Collects ethtool interface stats & registry dump",
+    "type": "Collector",
+    "description": "Collects ethtool interface stats & registry dump",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-ethtool"
   },
   {
     "name": "Facter",
-    "type": "collector",
-    "desription": "Collects system information via Facter facts",
+    "type": "Collector",
+    "description": "Collects system information via Facter facts",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-facter"
   },
   {
     "name": "Glance",
-    "type": "collector",
-    "desription": "Collects OpenStack Glance metrics",
+    "type": "Collector",
+    "description": "Collects OpenStack Glance metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-glance"
   },
   {
     "name": "HAproxy",
-    "type": "collector",
-    "desription": "Collects HAProxy performance metrics",
+    "type": "Collector",
+    "description": "Collects HAProxy performance metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-haproxy"
   },
   {
     "name": "InfluxDB",
-    "type": "collector",
-    "desription": "Collects InfluxDB internal monitoring metrics",
+    "type": "Collector",
+    "description": "Collects InfluxDB internal monitoring metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-influxdb"
   },
   {
     "name": "Interface",
-    "type": "collector",
-    "desription": "Collects network interface metrics from /proc/interface",
+    "type": "Collector",
+    "description": "Collects network interface metrics from /proc/interface",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-interface"
   },
   {
     "name": "IOstat",
-    "type": "collector",
-    "desription": "Collects Linux iostat metrics",
+    "type": "Collector",
+    "description": "Collects Linux iostat metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-iostat"
   },
   {
     "name": "Keystone",
-    "type": "collector",
-    "desription": "Collects OpenStack Keystone module metrics",
+    "type": "Collector",
+    "description": "Collects OpenStack Keystone module metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-keystone"
   },
   {
     "name": "Libvirt",
-    "type": "collector",
-    "desription": "Collects Linux libvirt metrics",
+    "type": "Collector",
+    "description": "Collects Linux libvirt metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-libvirt"
   },
   {
     "name": "Load",
-    "type": "collector",
-    "desription": "Collects Linux load average from /proc/loadavg",
+    "type": "Collector",
+    "description": "Collects Linux load average from /proc/loadavg",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-load"
   },
   {
     "name": "Meminfo",
-    "type": "collector",
-    "desription": "Collects Linux memory metrics from /proc/meminfo",
+    "type": "Collector",
+    "description": "Collects Linux memory metrics from /proc/meminfo",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-meminfo"
   },
   {
     "name": "Mesos",
-    "type": "collector",
-    "desription": "Collects Apache Mesos cluster metrics",
+    "type": "Collector",
+    "description": "Collects Apache Mesos cluster metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-mesos"
   },
   {
     "name": "MySQL",
-    "type": "collector",
-    "desription": "Collects MySQL database metrics",
+    "type": "Collector",
+    "description": "Collects MySQL database metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-mysql"
   },
   {
     "name": "Neutron",
-    "type": "collector",
-    "desription": "Collects OpenStack Neutron module metrics",
+    "type": "Collector",
+    "description": "Collects OpenStack Neutron module metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-neutron"
   },
   {
     "name": "Nfsclient",
-    "type": "collector",
-    "desription": "Collects NFS client metrics",
+    "type": "Collector",
+    "description": "Collects NFS client metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-nfsclient"
   },
   {
     "name": "Node-manager",
-    "type": "collector",
-    "desription": "Collects Intel Node Manager performance metrics",
+    "type": "Collector",
+    "description": "Collects Intel Node Manager performance metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-node-manager"
   },
   {
     "name": "Nova",
-    "type": "collector",
-    "desription": "Collects OpenStack Nova module metrics",
+    "type": "Collector",
+    "description": "Collects OpenStack Nova module metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-nova"
   },
   {
     "name": "Openfoam",
-    "type": "collector",
-    "desription": "Collects OpenFOAM performance metrics",
+    "type": "Collector",
+    "description": "Collects OpenFOAM performance metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-openfoam"
   },
   {
     "name": "OSv",
-    "type": "collector",
-    "desription": "Collects osv performance metrics",
+    "type": "Collector",
+    "description": "Collects osv performance metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-osv"
   },
   {
     "name": "PCM",
-    "type": "collector",
-    "desription": "Collects Intel Performance Counter Metrics (PCM)",
+    "type": "Collector",
+    "description": "Collects Intel Performance Counter Metrics (PCM)",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-pcm"
   },
   {
     "name": "Perfevents",
-    "type": "collector",
-    "desription": "Collects Linux cgroups perf metrics",
+    "type": "Collector",
+    "description": "Collects Linux cgroups perf metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-perfevents"
   },
   {
     "name": "Processes",
-    "type": "collector",
-    "desription": "Collects Linux process metrics from /proc/processes",
+    "type": "Collector",
+    "description": "Collects Linux process metrics from /proc/processes",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-processes"
   },
   {
     "name": "PSUtil",
-    "type": "collector",
-    "desription": "Collects Linux psutil metrics",
+    "type": "Collector",
+    "description": "Collects Linux psutil metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-psutil"
   },
   {
     "name": "RabbitMQ",
-    "type": "collector",
-    "desription": "Collects RabbitMQ metrics from management API",
+    "type": "Collector",
+    "description": "Collects RabbitMQ metrics from management API",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-rabbitmq"
   },
   {
     "name": "Scaleio",
-    "type": "collector",
-    "desription": "Collects ScaleIO performance metrics",
+    "type": "Collector",
+    "description": "Collects ScaleIO performance metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-scaleio"
   },
   {
     "name": "Smart",
-    "type": "collector",
-    "desription": "Collects Intel SSDs S.M.A.R.T. metrics",
+    "type": "Collector",
+    "description": "Collects Intel SSDs S.M.A.R.T. metrics",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-smart"
   },
   {
     "name": "Swap",
-    "type": "collector",
-    "desription": "Collects Linux swap metrics from /proc",
+    "type": "Collector",
+    "description": "Collects Linux swap metrics from /proc",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-swap"
   },
   {
     "name": "Users",
-    "type": "collector",
-    "desription": "Collects Linux user metrics from utmp",
+    "type": "Collector",
+    "description": "Collects Linux user metrics from utmp",
     "url": "https://github.com/intelsdi-x/snap-plugin-collector-users"
   },
   {
     "name": "Movingaverage",
-    "type": "processor",
-    "desription": "Process snap metrics and return a moving average",
+    "type": "Processor",
+    "description": "Process snap metrics and return a moving average",
     "url": "https://github.com/intelsdi-x/snap-plugin-processor-movingaverage"
   },
   {
     "name": "Tag",
-    "type": "processor",
-    "desription": "Process snap metrics and add tags to label the data",
+    "type": "Processor",
+    "description": "Process snap metrics and add tags to label the data",
     "url": "https://github.com/intelsdi-x/snap-plugin-processor-tag"
   },
   {
     "name": "File",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to a file",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to a file",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-file"
   },
   {
     "name": "Graphite",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to Graphite",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to Graphite",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-graphite"
   },
   {
     "name": "HANA",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to SAP Hana",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to SAP Hana",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-hana"
   },
   {
     "name": "HEKA",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to Mozilla HEKA",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to Mozilla HEKA",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-heka"
   },
   {
     "name": "InfluxDB",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to InfluxDB",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to InfluxDB",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-influxdb"
   },
   {
     "name": "Kafka",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to Apache Kafka",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to Apache Kafka",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-kafka"
   },
   {
     "name": "Kairosdb",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to KairosDB",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to KairosDB",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-kairosdb"
   },
   {
     "name": "MySQL",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to MySQL",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to MySQL",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-mysql"
   },
   {
     "name": "OpenTSDB",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to OpenTSDB",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to OpenTSDB",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-opentsdb"
   },
   {
     "name": "PostgreSQL",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to PostreSQL",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to PostreSQL",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-postgresql"
   },
   {
     "name": "RabbitMQ",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to RabbitMQ",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to RabbitMQ",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-rabbitmq"
   },
   {
     "name": "Riemann",
-    "type": "publisher",
-    "desription": "Publishes snap metrics to Riemann",
+    "type": "Publisher",
+    "description": "Publishes snap metrics to Riemann",
     "url": "https://github.com/intelsdi-x/snap-plugin-publisher-riemann"
   }
 ]
