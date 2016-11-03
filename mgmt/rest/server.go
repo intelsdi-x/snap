4152004b1eccef01feab533d6c0c69aa2f17cef3
kindermoumoute <kindermoumoute@gmail.com>
2016-11-03 14:39:42 -0700

    Update glide.lock and routes

--- mgmt/rest/server.go
+++ mgmt/rest/server.go
@@ -510,33 +510,37 @@ func (s *Server) addRoutes() {
 
 	// profiling tools routes
 	if s.pprof {
-		s.r.GET("/v1/debug/pprof/", s.index)
-		s.r.GET("/v1/debug/pprof/cmdline", s.cmdline)
-		s.r.GET("/v1/debug/pprof/profile", s.profile)
-		s.r.GET("/v1/debug/pprof/symbol", s.symbol)
-		s.r.GET("/v1/debug/pprof/trace", s.trace)
+		s.r.GET("/debug/pprof/", s.index)
+		s.r.GET("/debug/pprof/block", s.index)
+		s.r.GET("/debug/pprof/goroutine", s.index)
+		s.r.GET("/debug/pprof/heap", s.index)
+		s.r.GET("/debug/pprof/threadcreate", s.index)
+		s.r.GET("/debug/pprof/cmdline", s.cmdline)
+		s.r.GET("/debug/pprof/profile", s.profile)
+		s.r.GET("/debug/pprof/symbol", s.symbol)
+		s.r.GET("/debug/pprof/trace", s.trace)
 	}
 }
 
 // profiling tools handlers
 
-func (s *Server) index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (s *Server) index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	pprof.Index(w, r)
 }
 
-func (s *Server) cmdline(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (s *Server) cmdline(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	pprof.Cmdline(w, r)
 }
 
-func (s *Server) profile(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (s *Server) profile(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	pprof.Profile(w, r)
 }
 
-func (s *Server) symbol(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (s *Server) symbol(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	pprof.Symbol(w, r)
 }
 
-func (s *Server) trace(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (s *Server) trace(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	pprof.Trace(w, r)
 }
 
