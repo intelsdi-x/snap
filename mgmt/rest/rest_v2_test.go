9823de4dbfdfe8602655a4c472521b4056809d9a
kindermoumoute <kindermoumoute@gmail.com>
2017-01-17 09:51:08 -0800

    Update medium tests

--- mgmt/rest/rest_v2_test.go
+++ mgmt/rest/rest_v2_test.go
@@ -22,17 +22,24 @@ limitations under the License.
 package rest
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
 	"testing"
-
 	log "github.com/Sirupsen/logrus"
-	"github.com/intelsdi-x/snap/mgmt/rest/v2/fixtures"
+	"github.com/intelsdi-x/snap/mgmt/rest/v2/mock"
 	"github.com/intelsdi-x/snap/plugin/helper"
+	"github.com/intelsdi-x/snap/core/cdata"
+	"github.com/intelsdi-x/snap/core/ctypes"
 	. "github.com/smartystreets/goconvey/convey"
+	"strings"
+	"bytes"
+	"encoding/json"
 )
 
 var (
@@ -73,15 +80,15 @@ func startV2API(cfg *mockConfig, testType string) *restAPIInstance {
 	r, _ := New(cfg.RestAPI)
 	switch testType {
 	case "plugin":
-		mockMetricManager := &fixtures.MockManagesMetrics{}
-		mockConfigManager := &fixtures.MockConfigManager{}
+		mockMetricManager := &mock.MockManagesMetrics{}
+		mockConfigManager := &mock.MockConfigManager{}
 		r.BindMetricManager(mockMetricManager)
 		r.BindConfigManager(mockConfigManager)
 	case "metric":
-		mockMetricManager := &fixtures.MockManagesMetrics{}
+		mockMetricManager := &mock.MockManagesMetrics{}
 		r.BindMetricManager(mockMetricManager)
 	case "task":
-		mockTaskManager := &fixtures.MockTaskManager{}
+		mockTaskManager := &mock.MockTaskManager{}
 		r.BindTaskManager(mockTaskManager)
 	}
 	go func(ch <-chan error) {
@@ -101,9 +108,361 @@ func startV2API(cfg *mockConfig, testType string) *restAPIInstance {
 	}
 }
 
+func TestV2Plugin(t *testing.T) {
+	r := startV2API(getDefaultMockConfig(), "plugin")
+	Convey("Test Plugin REST API V2", t, func() {
+
+		Convey("Post plugins - v2/plugins/:type:name", func(c C) {
+			f, err := os.Open(MOCK_PLUGIN_PATH1)
+			defer f.Close()
+			So(err, ShouldBeNil)
+
+			// We create a pipe so that we can write the file in multipart
+			// format and read it in to the body of the http request
+			reader, writer := io.Pipe()
+			mwriter := multipart.NewWriter(writer)
+			bufin := bufio.NewReader(f)
+
+			// A go routine is needed since we must write the multipart file
+			// to the pipe so we can read from it in the http call
+			go func() {
+				part, err := mwriter.CreateFormFile("snap-plugins", "mock")
+				c.So(err, ShouldBeNil)
+				bufin.WriteTo(part)
+				mwriter.Close()
+				writer.Close()
+			}()
+
+			resp1, err1 := http.Post(
+				fmt.Sprintf("http://localhost:%d/v2/plugins", r.port),
+				mwriter.FormDataContentType(), reader)
+			So(err1, ShouldBeNil)
+			So(resp1.StatusCode, ShouldEqual, 201)
+		})
+
+		Convey("Get plugins - v2/plugins", func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/plugins", r.port))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.GET_PLUGINS_RESPONSE, r.port, r.port,
+					r.port, r.port, r.port, r.port))
+		})
+		Convey("Get plugins - v2/plugins/:type", func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/plugins/collector", r.port))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.GET_PLUGINS_RESPONSE_TYPE, r.port, r.port))
+		})
+		Convey("Get plugins - v2/plugins/:type:name", func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/plugins/publisher/bar", r.port))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.GET_PLUGINS_RESPONSE_TYPE_NAME, r.port))
+		})
+		Convey("Get plugin - v2/plugins/:type:name:version", func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/plugins/publisher/bar/3", r.port))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.GET_PLUGINS_RESPONSE_TYPE_NAME_VERSION, r.port))
+		})
+
+		Convey("Delete plugins - v2/plugins/:type:name:version", func() {
+			c := &http.Client{}
+			pluginName := "foo"
+			pluginType := "collector"
+			pluginVersion := 2
+			req, err := http.NewRequest(
+				"DELETE",
+				fmt.Sprintf("http://localhost:%d/v2/plugins/%s/%s/%d",
+					r.port,
+					pluginType,
+					pluginName,
+					pluginVersion),
+				bytes.NewReader([]byte{}))
+			So(err, ShouldBeNil)
+			resp, err := c.Do(req)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusNoContent)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.UNLOAD_PLUGIN_RESPONSE))
+		})
+
+		Convey("Get plugin config items - v2/plugins/:type/:name/:version/config", func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/plugins/publisher/bar/3/config", r.port))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.GET_PLUGIN_CONFIG_ITEM))
+		})
+
+		Convey("Set plugin config item- v2/plugins/:type/:name/:version/config", func() {
+			c := &http.Client{}
+			pluginName := "foo"
+			pluginType := "collector"
+			pluginVersion := 2
+			cd := cdata.NewNode()
+			cd.AddItem("user", ctypes.ConfigValueStr{Value: "Jane"})
+			body, err := cd.MarshalJSON()
+			So(err, ShouldBeNil)
+
+			req, err := http.NewRequest(
+				"PUT",
+				fmt.Sprintf("http://localhost:%d/v2/plugins/%s/%s/%d/config",
+					r.port,
+					pluginType,
+					pluginName,
+					pluginVersion),
+				bytes.NewReader(body))
+			So(err, ShouldBeNil)
+			resp, err := c.Do(req)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusOK)
+			body, err = ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.SET_PLUGIN_CONFIG_ITEM))
+
+		})
+
+		Convey("Delete plugin config item - /v2/plugins/:type/:name/:version/config", func() {
+			c := &http.Client{}
+			pluginName := "foo"
+			pluginType := "collector"
+			pluginVersion := 2
+			cd := []string{"foo"}
+			body, err := json.Marshal(cd)
+			So(err, ShouldBeNil)
+			req, err := http.NewRequest(
+				"DELETE",
+				fmt.Sprintf("http://localhost:%d/v2/plugins/%s/%s/%d/config",
+					r.port,
+					pluginType,
+					pluginName,
+					pluginVersion),
+				bytes.NewReader(body))
+
+			So(err, ShouldBeNil)
+			resp, err := c.Do(req)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusOK)
+			body, err = ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.DELETE_PLUGIN_CONFIG_ITEM))
+		})
+	})
+}
+
+func TestV2Task(t *testing.T) {
+	r := startV2API(getDefaultMockConfig(), "task")
+	Convey("Test Task REST API V2", t, func() {
+
+		Convey("Add tasks - v2/tasks", func() {
+			reader := strings.NewReader(mock.TASK)
+			resp, err := http.Post(
+				fmt.Sprintf("http://localhost:%d/v2/tasks", r.port),
+				http.DetectContentType([]byte(mock.TASK)),
+				reader)
+			So(err, ShouldBeNil)
+			So(resp, ShouldNotBeEmpty)
+			So(resp.StatusCode, ShouldEqual, 201)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				fmt.Sprintf(mock.ADD_TASK_RESPONSE, r.port),
+				ShouldResemble,
+				string(body))
+		})
+
+		Convey("Get tasks - v2/tasks", func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/tasks", r.port))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			responses := []string{
+				fmt.Sprintf(mock.GET_TASKS_RESPONSE, r.port, r.port),
+				fmt.Sprintf(mock.GET_TASKS_RESPONSE2, r.port, r.port),
+			}
+			// GetTasks returns an unordered map,
+			// thus there is more than one possible response
+			So(
+				responses,
+				ShouldContain,
+				string(body))
+		})
+
+		Convey("Get task - v2/tasks/:id", func() {
+			taskID := "1234"
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/tasks/:%s", r.port, taskID))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				fmt.Sprintf(mock.GET_TASK_RESPONSE, r.port),
+				ShouldResemble,
+				string(body))
+		})
+
+		Convey("Watch tasks - v2/tasks/:id/watch", func() {
+			taskID := "1234"
+			resp, err := http.Get(
+				fmt.Sprintf("http://localhost:%d/v2/tasks/:%s/watch", r.port, taskID))
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusOK)
+		})
+
+		Convey("Start tasks - v2/tasks/:id", func() {
+			c := &http.Client{}
+			taskID := "MockTask1234"
+			cd := cdata.NewNode()
+			cd.AddItem("user", ctypes.ConfigValueStr{Value: "Kelly"})
+			body, err := cd.MarshalJSON()
+			So(err, ShouldBeNil)
+
+			req, err := http.NewRequest(
+				"PUT",
+				fmt.Sprintf("http://localhost:%d/v2/tasks/%s", r.port, taskID),
+				bytes.NewReader(body))
+			So(err, ShouldBeNil)
+			q := req.URL.Query()
+			q.Add("action", "start")
+			req.URL.RawQuery = q.Encode()
+			resp, err := c.Do(req)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusNoContent)
+			body, err = ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.START_TASK_RESPONSE_ID_START))
+		})
+
+		Convey("Stop tasks - v2/tasks/:id", func() {
+			c := &http.Client{}
+			taskID := "MockTask1234"
+			cd := cdata.NewNode()
+			cd.AddItem("user", ctypes.ConfigValueStr{Value: "Kelly"})
+			body, err := cd.MarshalJSON()
+			So(err, ShouldBeNil)
+
+			req, err := http.NewRequest(
+				"PUT",
+				fmt.Sprintf("http://localhost:%d/v2/tasks/%s", r.port, taskID),
+				bytes.NewReader(body))
+			So(err, ShouldBeNil)
+			q := req.URL.Query()
+			q.Add("action", "stop")
+			req.URL.RawQuery = q.Encode()
+			resp, err := c.Do(req)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusNoContent)
+			body, err = ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.STOP_TASK_RESPONSE_ID_STOP))
+		})
+
+		Convey("Enable tasks - v2/tasks/:id", func() {
+			c := &http.Client{}
+			taskID := "MockTask1234"
+			cd := cdata.NewNode()
+			cd.AddItem("user", ctypes.ConfigValueStr{Value: "Kelly"})
+			body, err := cd.MarshalJSON()
+			So(err, ShouldBeNil)
+
+			req, err := http.NewRequest(
+				"PUT",
+				fmt.Sprintf("http://localhost:%d/v2/tasks/%s", r.port, taskID),
+				bytes.NewReader(body))
+			So(err, ShouldBeNil)
+			q := req.URL.Query()
+			q.Add("action", "enable")
+			req.URL.RawQuery = q.Encode()
+			resp, err := c.Do(req)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusNoContent)
+			body, err = ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.ENABLE_TASK_RESPONSE_ID_ENABLE))
+		})
+
+		Convey("Remove tasks - v2/tasks/:id", func() {
+			c := &http.Client{}
+			taskID := "MockTask1234"
+			cd := []string{"foo"}
+			body, err := json.Marshal(cd)
+			So(err, ShouldBeNil)
+			req, err := http.NewRequest(
+				"DELETE",
+				fmt.Sprintf("http://localhost:%d/v2/tasks/%s",
+					r.port,
+					taskID),
+				bytes.NewReader([]byte{}))
+			So(err, ShouldBeNil)
+			resp, err := c.Do(req)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode, ShouldEqual, http.StatusNoContent)
+			body, err = ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(
+				string(body),
+				ShouldResemble,
+				fmt.Sprintf(mock.REMOVE_TASK_RESPONSE_ID))
+		})
+	})
+}
+
 func TestV2Metric(t *testing.T) {
 	r := startV2API(getDefaultMockConfig(), "metric")
 	Convey("Test Metric REST API V2", t, func() {
+
 		Convey("Get metrics - v2/metrics", func() {
 			resp, err := http.Get(
 				fmt.Sprintf("http://localhost:%d/v2/metrics", r.port))
@@ -116,32 +475,22 @@ func TestV2Metric(t *testing.T) {
 			So(
 				resp1,
 				ShouldResemble,
-				fmt.Sprintf(fixtures.GET_METRICS_RESPONSE, r.port))
+				fmt.Sprintf(mock.GET_METRICS_RESPONSE, r.port))
 		})
-	})
-}
 
-func TestV2Task(t *testing.T) {
-	r := startV2API(getDefaultMockConfig(), "task")
-	Convey("Test Task REST API V2", t, func() {
-		Convey("Get tasks - v2/tasks", func() {
+		Convey("Get metrics from tree - v2/metrics/*namespace", func() {
 			resp, err := http.Get(
-				fmt.Sprintf("http://localhost:%d/v2/tasks", r.port))
+				fmt.Sprintf("http://localhost:%d/v2/metrics/*namespace", r.port))
 			So(err, ShouldBeNil)
 			So(resp.StatusCode, ShouldEqual, 200)
 			body, err := ioutil.ReadAll(resp.Body)
 			So(err, ShouldBeNil)
-			responses := []string{
-				fmt.Sprintf(fixtures.GET_TASKS_RESPONSE, r.port, r.port),
-				fmt.Sprintf(fixtures.GET_TASKS_RESPONSE2, r.port, r.port),
-			}
-			// GetTasks returns an unordered map,
-			// thus there is more than one possible response
+			resp1, err := url.QueryUnescape(string(body))
+			So(err, ShouldBeNil)
 			So(
-				responses,
-				ShouldContain,
-				string(body))
+				fmt.Sprintf(mock.GET_METRICS_RESPONSE, r.port),
+				ShouldResemble,
+				resp1)
 		})
-
 	})
 }
