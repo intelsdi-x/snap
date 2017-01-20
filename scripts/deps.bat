0fd064ba6dc4c78c181feb69e53f43bfe49cb167
Jesse Millar <hellojessemillar@gmail.com>
2017-01-20 14:30:11 -0700

    Cleaned up deps.bat

--- scripts/deps.bat
+++ scripts/deps.bat
@@ -1,12 +1,8 @@
-@ECHO OFF
-ECHO Gathering Dependencies...
-SET _proj_dir="%~dp0.."
-CD /D %_proj_dir%
+@echo off
+echo Gathering Dependencies...
+set _proj_dir="%~dp0.."
+cd %_proj_dir%
 go get github.com/Masterminds/glide
-ECHO Retrieved Dependency: Glide
-setx path "%PATH%;C:\New Folder" 
-:: glide install is erroring out.
-:: install glide manually rather than go get?
-CD %_proj_dir% && glide install
-::Add more dependencies here
-ECHO Finished Gathering Dependencies...
+echo Retrieved Dependency: Glide
+glide install
+echo Finished Gathering Dependencies...
