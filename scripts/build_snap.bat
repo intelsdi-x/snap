6d882e64bad5ba895fd526e9b877e294720165d4
Jesse Millar <hellojessemillar@gmail.com>
2017-01-20 15:55:43 -0700

    Minor tweaks to build_snap.bat but still not sure why things are broken

--- scripts/build_snap.bat
+++ scripts/build_snap.bat
@@ -1,11 +1,10 @@
 @echo OFF
-setlocal
 
 echo Building Snap...
 set _proj_dir=%~dp0..
 for /f "tokens=1-3" %%i in ('git --version') do set git_version=%%k
 set go_build=go build -ldflags "-w -X main.gitversion=%git_version%"
-set CGO_ENABLED=1
+set CGO_ENABLED=0
 set GOOS=windows
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set GOARCH=386 || set GOARCH=amd64
 
@@ -18,9 +17,9 @@ if %GOARCH%==amd64 (
 md %build_path%
 
 cd %_proj_dir%
-%go_build% -o "%build_path%\snapteld.exe" . || exit /B 1
+%go_build% -o "%build_path%\snapteld.exe" snapteld.go || exit /B 1
 
 cd %_proj_dir%\cmd\snaptel
-%go_build% -o "%build_path%\snaptel.exe" . || exit /B 1
+%go_build% -o "%build_path%\snaptel.exe" snaptel.go || exit /B 1
 
 echo Built Snap...
