16da9b27796247b2021653c3a052c63e5c167d96
kindermoumoute <kindermoumoute@gmail.com>
2017-01-24 11:33:42 -0800

    Add API interface

---
+++ mgmt/rest/api/tribe.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"github.com/intelsdi-x/snap/core/serror"
+	"github.com/intelsdi-x/snap/mgmt/tribe/agreement"
+)
+
+type Tribe interface {
+	GetAgreement(name string) (*agreement.Agreement, serror.SnapError)
+	GetAgreements() map[string]*agreement.Agreement
+	AddAgreement(name string) serror.SnapError
+	RemoveAgreement(name string) serror.SnapError
+	JoinAgreement(agreementName, memberName string) serror.SnapError
+	LeaveAgreement(agreementName, memberName string) serror.SnapError
+	GetMembers() []string
+	GetMember(name string) *agreement.Member
+}
