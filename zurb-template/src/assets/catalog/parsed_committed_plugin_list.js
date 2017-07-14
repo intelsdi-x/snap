08a18935577a124f68e279278dc8389f921dc8d4
Sarah <sarahhan88@gmail.com>
2017-07-14 16:03:41 -0700

    update snap banner on landing page

--- zurb-template/src/assets/catalog/parsed_committed_plugin_list.js
+++ zurb-template/src/assets/catalog/parsed_committed_plugin_list.js
@@ -7,23 +7,9 @@ myfcn2(
 "github_id": "@sandlbn"
 },
 {
-"name": "Cassandra",
-"type": "Collector",
-"description": "Collects metrics from Cassandra cluster",
-"author_url": "https://github.com/candysmurf",
-"github_id": "@candysmurf"
-},
-{
 "name": "Redfish",
 "type": "Collector",
 "description": "Collects metrics from Redfish API",
-"author_url": "https://github.com/candysmurf",
-"github_id": "@candysmurf"
-},
-{
-"name": "Cassandra",
-"type": "Publisher",
-"description": "Publishes snap metrics into Cassandra",
 "author_url": "https://github.com/candysmurf",
 "github_id": "@candysmurf"
 }]
